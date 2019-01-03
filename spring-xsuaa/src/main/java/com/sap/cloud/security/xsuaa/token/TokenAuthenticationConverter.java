a35ffa96711daea1441183201e7c1dfc387e33ae
Nena Raab <nena.raab@sap.com>
2019-01-03 11:48:00 +0100

    Ease enhancement of TokenAuthenticationConverter #23

--- spring-xsuaa/src/main/java/com/sap/cloud/security/xsuaa/token/TokenAuthenticationConverter.java
+++ spring-xsuaa/src/main/java/com/sap/cloud/security/xsuaa/token/TokenAuthenticationConverter.java
@@ -1,42 +1,58 @@
+/**
+ * Copyright (c) 2018 SAP SE or an SAP affiliate company. All rights reserved.
+ * This file is licensed under the Apache Software License,
+ * v. 2 except as noted otherwise in the LICENSE file
+ * https://github.com/SAP/cloud-security-xsuaa-integration/blob/master/LICENSE
+ */
 package com.sap.cloud.security.xsuaa.token;
 
-import com.sap.cloud.security.xsuaa.XsuaaServiceConfiguration;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import com.sap.cloud.security.xsuaa.XsuaaServiceConfiguration;
 
+/**
+ * Converter for xsuaa jwt token that stores authorization data like scopes inside the token.
+ */
 public class TokenAuthenticationConverter implements Converter<Jwt, AbstractAuthenticationToken> {
 
-    private String appId;
+	protected String appId;
 
-    public TokenAuthenticationConverter(String appId) {
-        this.appId = appId;
-    }
+	public TokenAuthenticationConverter(String appId) {
+		this.appId = appId;
+	}
 
-    public TokenAuthenticationConverter(XsuaaServiceConfiguration xsuaaServiceConfiguration) {
-        this.appId = xsuaaServiceConfiguration.getAppId();
-    }
+	public TokenAuthenticationConverter(XsuaaServiceConfiguration xsuaaServiceConfiguration) {
+		this.appId = xsuaaServiceConfiguration.getAppId();
+	}
 
+	public final AbstractAuthenticationToken convert(Jwt jwt) {
+		return new AuthenticationToken(appId, jwt, extractAuthorities(jwt));
+	}
 
-    public final AbstractAuthenticationToken convert(Jwt jwt) {
-        Collection<GrantedAuthority> authorities = extractAuthorities(jwt);
-        return new AuthenticationToken(appId, jwt, authorities);
-    }
+	protected Collection<GrantedAuthority> extractAuthorities(Jwt jwt) {
+		Collection<String> scopeAuthorities = getScopes(jwt);
+		Collection<String> customAuthorities = getCustomAuthorities(new TokenImpl(jwt, appId));
 
-    protected Collection<GrantedAuthority> extractAuthorities(Jwt jwt) {
-        Collection<String> scopes = this.getScopes(jwt);
-        return scopes.stream().map(authority -> authority).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
-    }
+		Stream<String> authorities = Stream.of(scopeAuthorities, customAuthorities).flatMap(Collection::stream);
+		return authorities.map(authority -> authority).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+	}
 
-    private Collection<String> getScopes(Jwt jwt) {
-        List<String> scopesList = jwt.getClaimAsStringList(Token.CLAIM_SCOPES);
-        return scopesList != null ? scopesList : Collections.emptyList();
-    }
+	protected Collection<String> getCustomAuthorities(Token token) {
+		return Collections.emptyList();
+	}
+
+	protected Collection<String> getScopes(Jwt jwt) {
+		List<String> scopesList = jwt.getClaimAsStringList(Token.CLAIM_SCOPES);
+		return scopesList != null ? scopesList : Collections.emptyList();
+	}
 }
