9e9eccf31a87a442f15ae001f3a8ba2cd65acce7
Nena Raab <nena.raab@sap.com>
2019-01-09 11:27:52 +0100

    initial version of spring-xsuaa-mock #30

---
+++ spring-xsuaa-mock/src/test/java/com/sap/cloud/security/xsuaa/mock/XsuaaMockWebServerTest.java
@@ -0,0 +1,23 @@
+package com.sap.cloud.security.xsuaa.mock;
+
+import static org.hamcrest.Matchers.startsWith;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class XsuaaMockWebServerTest {
+	private XsuaaMockWebServer xsuaaMockServer;
+
+	@Before
+	public void setUp() {
+		xsuaaMockServer = new XsuaaMockWebServer();
+	}
+
+	@Test
+	public void getPropertyShouldStartMockServerAndReturnUrl() {
+		String url = (String) xsuaaMockServer.getProperty(XsuaaMockWebServer.MOCK_XSUAA_PROPERTY_SOURCE_NAME);
+		url = url.replace("127.0.0.1", "localhost");
+		Assert.assertThat(url, startsWith("http://localhost"));
+	}
+}
