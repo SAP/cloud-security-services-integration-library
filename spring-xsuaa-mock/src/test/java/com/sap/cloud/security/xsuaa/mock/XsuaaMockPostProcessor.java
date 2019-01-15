f55e190682afb952149df3eef549adf76c726ace
Nena Raab <nena.raab@sap.com>
2019-01-15 18:15:56 +0100

    spring-xsuaa-mock: fix junit test

--- spring-xsuaa-mock/src/test/java/com/sap/cloud/security/xsuaa/mock/XsuaaMockPostProcessor.java
+++ spring-xsuaa-mock/src/test/java/com/sap/cloud/security/xsuaa/mock/XsuaaMockPostProcessor.java
@@ -10,8 +10,6 @@ import org.springframework.http.HttpStatus;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.RecordedRequest;
 
-import java.nio.charset.StandardCharsets;
-
 public class XsuaaMockPostProcessor implements EnvironmentPostProcessor, DisposableBean {
 
 	private final XsuaaMockWebServer mockAuthorizationServer;
@@ -37,8 +35,9 @@ public class XsuaaMockPostProcessor implements EnvironmentPostProcessor, Disposa
 		@Override
 		public MockResponse dispatch(RecordedRequest request) {
 			if ("/customdomain/token_keys".equals(request.getPath())) {
-				return getTokenKeyForKeyId(PATH_TOKEN_KEYS_TEMPLATE, PATH_PUBLIC_KEY, "legacy-token-key-customdomain");
-			} if ("/testdomain/token_keys".equals(request.getPath())) {
+				return getTokenKeyForKeyId(PATH_TOKEN_KEYS_TEMPLATE, "legacy-token-key-customdomain");
+			}
+			if ("/testdomain/token_keys".equals(request.getPath())) {
 				return getResponseFromFile("/mock/testdomain_token_keys.json", HttpStatus.OK);
 			}
 			return super.dispatch(request);
