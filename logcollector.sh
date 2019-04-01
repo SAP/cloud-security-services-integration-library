464a8affd68a32c256e46c0b59de26245f1562d0
Alessandro Stoltenberg <alessandro.stoltenberg@sap.com>
2019-04-01 19:55:24 +0200

    Check if cf cli is available.

--- logcollector.sh
+++ logcollector.sh
@@ -1,1 +1,4 @@
 #! /usr/bin/env bash
+
+#Checking if cf-cli is installed
+hash cf 2>/dev/null || { echo >&2 "cf command line client not found, please install cf cli first (https://github.com/cloudfoundry/cli#downloads)."; exit 1; } 
