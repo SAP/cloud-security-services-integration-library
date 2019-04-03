cf8639de97be9b1e4bdad89904d6c267e00f228e
Alessandro Stoltenberg <alessandro.stoltenberg@sap.com>
2019-04-03 13:40:59 +0200

    Working script for bash

--- logcollector.sh
+++ logcollector.sh
@@ -1,20 +1,25 @@
 #! /usr/bin/env bash
 
-echo -e "\nLogging in...\n"
-
 #functions
-usage() { echo >&2 -e "Usage: ./logcollector.sh <app-name> [<approuter-name]>]"; exit 0; }
+usage() { echo >&2 -e "Usage: ./logcollector.sh <app-name> <approuter-name> [output-file]\nIf no output file is specified $HOME/logcollection.zip will be used."; exit 0; }
 
 checkappname() {
-	cf app "$1" --guid &>/dev/null || { echo -e >&2 "\nApp/Approuter $1 not found, did you target the correct space?"; exit 1; }
-}       
+	cf app "$1" --guid &>/dev/null || { echo -e >&2 "\nApp/Approuter \"$1\" not found, did you target the correct space?"; exit 1; }
+}
 
 #Variables
 appname="$1"
-approutername="$1-approuter"
+approutername="$2"
+logszip="$HOME/logcollection.zip"
+logstxt="/tmp/logcollection.txt"
+
+if [[ -n $3 ]]
+then
+	logszip="$3"
+fi
 
 #Check number of args
-if [[ $# -eq 0 || $# -gt 2 ]]
+if [[ $# -eq 0 || $# -gt 3 ]]
 then
 	usage
 	exit 1
@@ -33,14 +38,48 @@ shift $((OPTIND-1))
 hash cf 2>/dev/null || { echo >&2 "cf command line client not found, please install cf cli first (https://github.com/cloudfoundry/cli#downloads)."; exit 1; }
 
 #login to the correct API endpoint
+echo -e "\nLogging in...\n"
 cf login || { echo -e >&2 "\nScript aborted due to failed login. Please check your credentials and try again."; exit 1; }
 
 echo -e "\nSuccessfully logged in, will continue..."
 
-checkappname "$1"
+checkappname "$appname"
+checkappname "$approutername"
 
-if [[ -n $2 ]]
-then
-	checkappname "$2"
-	approutername="$2"
-fi
+#Set the enviroment variables and restart the apps
+echo -e "\nSetting log levels...\n"
+cf set-env "$approutername" XS_APP_LOG_LEVEL DEBUG
+cf set-env "$appname" SAP_EXT_TRC stdout
+cf set-env "$appname" SAP_EXT_TRL 3
+cf set-env "$appname" DEBUG xssec*
+
+echo -e "\nRestart the app and the approuter...\n"
+cf restart "$approutername"
+cf restart "$appname"
+
+#Creating, collecting and compressing the logs
+echo -e "\n\e[36m\e[1mNow please repeat your scenario (e.g. try to login to your app or similar)...\e[0m\n"
+read -rp "When you are done please press ENTER to collect the logs..."
+
+echo -e "\nCollecting the logs..."
+
+echo -e "Approuter logs:\n\n" >"$logstxt"
+cf logs "$approutername" --recent >>"$logstxt"
+echo -e "App logs:\n\n" >>"$logstxt"
+cf logs "$appname" --recent >>"$logstxt"
+zip "$logszip" "$logstxt" >/dev/null
+rm "$logstxt"
+
+#Unsetting env variables and restarting apps
+echo -e "\nRestoring log levels...\n"
+cf unset-env "$approutername" XS_APP_LOG_LEVEL
+cf unset-env "$appname" SAP_EXT_TRC
+cf unset-env "$appname" SAP_EXT_TRL
+cf unset-env "$appname" DEBUG
+
+echo -e "\nRestart the app and the approuter...\n"
+cf restart "$approutername"
+cf restart "$appname"
+
+#End
+echo -e "\n\e[32m\e[1mAll done.\e[0m Your file is here:" && readlink -f "$logszip"
