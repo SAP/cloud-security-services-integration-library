c049eb1ff15abbf036f9339771512ac8b41ff1f8
Nena Raab <nena.raab@sap.com>
2019-05-03 12:08:35 +0200

    remove unused imports

--- spring-xsuaa-mock/src/main/java/com/sap/cloud/security/xsuaa/mock/MockXsuaaServiceConfiguration.java
+++ spring-xsuaa-mock/src/main/java/com/sap/cloud/security/xsuaa/mock/MockXsuaaServiceConfiguration.java
@@ -3,9 +3,6 @@ package com.sap.cloud.security.xsuaa.mock;
 import org.springframework.beans.factory.annotation.Value;
 
 import com.sap.cloud.security.xsuaa.XsuaaServiceConfigurationDefault;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
 
 public class MockXsuaaServiceConfiguration extends XsuaaServiceConfigurationDefault {
 
