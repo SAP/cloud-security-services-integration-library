0c774d956fc69aafbff25b36f66f1130d6f2dd79
RRoggia <renanzr@gmail.com>
2019-05-09 18:13:38 -0300

    change NimbusJwtDecoder to NimbusReactiveJwtDecoder

--- spring-xsuaa/src/main/java/com/sap/cloud/security/xsuaa/token/authentication/ReactiveXsuaaJwtDecoder.java
+++ spring-xsuaa/src/main/java/com/sap/cloud/security/xsuaa/token/authentication/ReactiveXsuaaJwtDecoder.java
@@ -6,10 +6,9 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.security.oauth2.jwt.JwtTimestampValidator;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoderJwkSupport;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 
 import com.github.benmanes.caffeine.cache.Cache;
@@ -24,7 +23,7 @@ import reactor.core.publisher.Mono;
 
 public class ReactiveXsuaaJwtDecoder implements ReactiveJwtDecoder {
 
-	Cache<String, JwtDecoder> cache;
+	Cache<String, ReactiveJwtDecoder> cache;
 	private XsuaaServiceConfiguration xsuaaServiceConfiguration;
 
 	ReactiveXsuaaJwtDecoder(XsuaaServiceConfiguration xsuaaServiceConfiguration, int cacheValidity, int cacheSize) {
@@ -60,7 +59,7 @@ public class ReactiveXsuaaJwtDecoder implements ReactiveJwtDecoder {
 			}
 		})).map(tuple -> {
 			return cache.get(tuple.getT1(), k -> this.getDecoder(tuple.getT2(), tuple.getT1()));
-		}).map(decoder -> decoder.decode(token));
+		}).flatMap(decoder -> decoder.decode(token));
 	}
 
 	protected String getSubdomain(JWT jwt) throws ParseException {
@@ -72,9 +71,9 @@ public class ReactiveXsuaaJwtDecoder implements ReactiveJwtDecoder {
 		return subdomain;
 	}
 
-	private JwtDecoder getDecoder(String zid, String subdomain) {
+	private ReactiveJwtDecoder getDecoder(String zid, String subdomain) {
 		String url = xsuaaServiceConfiguration.getTokenKeyUrl(zid, subdomain);
-		NimbusJwtDecoderJwkSupport decoder = new NimbusJwtDecoderJwkSupport(url);
+		NimbusReactiveJwtDecoder decoder = new NimbusReactiveJwtDecoder(url);
 		OAuth2TokenValidator<Jwt> validators = new DelegatingOAuth2TokenValidator<>(new JwtTimestampValidator(),
 				new XsuaaAudienceValidator(xsuaaServiceConfiguration));
 		decoder.setJwtValidator(validators);
