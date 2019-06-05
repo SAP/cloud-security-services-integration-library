5618ed42204c56db6f863d1d4d4804befae11956
TheFonz2017 <31238517+TheFonz2017@users.noreply.github.com>
2019-06-05 12:02:05 +0200

    fixes.

--- spring-xsuaa/src/main/java/com/sap/cloud/security/xsuaa/XsuaaServiceBindings.java
+++ spring-xsuaa/src/main/java/com/sap/cloud/security/xsuaa/XsuaaServiceBindings.java
@@ -29,9 +29,9 @@ public interface XsuaaServiceBindings extends Serializable {
         "label":         "xsuaa",
         "provider":      null,
         "plan":          "application",
-        "name":          "d056076-netflix-xsuaa",
+        "name":          "some-xsuaa",
         "tags":          ["xsuaa"],
-        "instance_name": "d056076-netflix-xsuaa",
+        "instance_name": "some-xsuaa",
         "binding_name":  null,
         "credentials" :  {...}
         */
@@ -62,11 +62,11 @@ public interface XsuaaServiceBindings extends Serializable {
             "clientid":        "sb-spring-netflix-demo!t12291",
             "verificationkey": "-----BEGIN PUBLIC KEY-----MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAwThn6OO9kj0bchkOGkqYBnV1dQ3zU/xtj7Kj7nDd8nyRMcEWCtVzrzjzhiisRhlrzlRIEY82wRAZNGKMnw7cvCwNixcfcDJnjzgr2pJ+5/yDZUc0IXXyIWPZD+XdL+0EogC3d4+fqyvg/BF/F0t2hKHWr/UTXE6zrGhBKaL0d8rKfYd6olGWigFd+3+24CKI14zWVxUBtC+P9Fhngc9DRzkXqhxOK/EKn0HzSgotf5duq6Tmk9DCNM4sLW4+ERc6xzrgbeEexakabvax/Az9WZ4qhwgw+fwIhKIC7WLwCEJaRsW4m7NKkv+eJR2LKYesuQ9SVAJ3EXV86RwdnH4uAv7lQHsKURPVAQBlranSqyQu0EXs2N9OlWTxe+FyNkIvyZvoLrZl/CdlYc8AKxRm5rn2/88nkrYQ0XZSrnICM5FRWgVF2hn5KfZGwtBN85/D4Yck6B3ocMfyX7e4URUm9lRPQFUJGTXaZnEIge0R159HUwhTN1HvyXrs6uT1ZZmW+c3p47dw1+LmUf/hIf8zd+uvHQjIeHEJqxjqfyA8yqAFKRHKVFrwnwdMHIsRap2EKBhHMfeVf0P2th5C9MggYoGCvdIaIUgMBX3TtCdvGrcWML7hnyS2zkrlA8SoKJnRcRF2KxWKs355FhpHpzqyZflO5l98+O8wOsFjGpL9d0ECAwEAAQ==-----END PUBLIC KEY-----",
             "xsappname":       "spring-netflix-demo!t12291",
-            "identityzone":    "d056076-sub1",
+            "identityzone":    "some-tenant",
             "identityzoneid":  "d22b9a7f-53b2-4f88-8298-cc51f86e7f68",
             "clientsecret":    "2Tc2Xz7DNy4KiACwvunulmxF32w=",
             "tenantid":        "d22b9a7f-53b2-4f88-8298-cc51f86e7f68",
-            "url":             "https://d056076-sub1.authentication.eu10.hana.ondemand.com"
+            "url":             "https://some-tenant.authentication.eu10.hana.ondemand.com"
         }
         */
     }
