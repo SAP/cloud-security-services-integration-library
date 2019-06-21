f1d13bb7797cefa1768b495484153d7d6c35cffd
Nena Raab <nena.raab@sap.com>
2019-06-21 18:40:42 +0200

    javadoc

--- spring-xsuaa/src/main/java/com/sap/cloud/security/xsuaa/token/AuthenticationToken.java
+++ spring-xsuaa/src/main/java/com/sap/cloud/security/xsuaa/token/AuthenticationToken.java
@@ -6,6 +6,13 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
+/**
+ * Internal class used to expose the {@link Token} implementation as the standard Principal for Spring
+ * Security Jwt handling.
+ *
+ * @see TokenAuthenticationConverter
+ * @see TokenImpl
+ */
 public class AuthenticationToken extends JwtAuthenticationToken {
 
 	public AuthenticationToken(Jwt jwt, Collection<GrantedAuthority> authorities) {
@@ -14,6 +21,8 @@ public class AuthenticationToken extends JwtAuthenticationToken {
 
 	@Override
 	public Object getPrincipal() {
+		// Here is where the actual magic happens.
+		// The Jwt is exchanged for another implementation.
 		TokenImpl token = new TokenImpl(getToken());
 		token.setAuthorities(this.getAuthorities());
 		return token;
