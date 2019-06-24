a038f83cba7160660a3e0f079778fdd81bd30a87
Nena Raab <nena.raab@sap.com>
2019-06-24 22:52:37 +0200

    update version script

---
+++ etc/update_version.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+pomVersion=0.0.0
+if [ "$#" -eq 1 ]; then
+  echo "Update com.sap.cloud.security.xsuaa version to: " $1
+  pomVersion=$1
+else
+  echo ""
+  echo "Call this script with a version, e.g. see pom.xml and use 1.n.0 (n=n+1)"
+  echo "Example: ./update_version.sh 1.6.0"
+  echo ""
+  exit 1
+fi
+################################# CONFIGURATION #################################
+cd ..
+git remote add sec-lib-spring git@github.com:SAP/cloud-security-xsuaa-integration.git
+git fetch sec-lib-spring
+git checkout -B master_version sec-lib-spring/master
+git reset --hard sec-lib-spring/master
+git clean -d -x -f
+sed -i 's/## master[\n]*$/## master\n\n## '$pomVersion'\n\n### Changed\n - \n/g' CHANGELOG.md
+mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$pomVersion
+mvn clean install -DskipTests=true -Dmaven.test.skip=true
+cd spring-xsuaa-it
+mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$pomVersion
+cd ..
+git commit -a -m "com.sap.cloud.security.xsuaa version $pomVersion"
+git push sec-lib-spring master_version
+echo "Next Steps:"
+echo "1. Update version in samples Version to $pomVersion"
+echo "2. git push sec-lib-spring master_version"
+echo "3. start PR from master_version"
+
