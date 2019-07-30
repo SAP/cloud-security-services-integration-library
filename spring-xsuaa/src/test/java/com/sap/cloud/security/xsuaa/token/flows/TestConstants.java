063aad0b27b678a94cf6294bd2b9152c565b003d
Nena Raab <nena.raab@sap.com>
2019-07-30 18:42:05 +0200

    TestConstants should not refer to valid oauth server; remove SystemTests

--- spring-xsuaa/src/test/java/com/sap/cloud/security/xsuaa/token/flows/TestConstants.java
+++ spring-xsuaa/src/test/java/com/sap/cloud/security/xsuaa/token/flows/TestConstants.java
@@ -6,9 +6,7 @@ import java.net.URI;
 interface TestConstants {
     String clientId          = "sb-spring-netflix-demo!t12291";
     String clientSecret      = "2Tc2Xz7DNy4KiACwvunulmxF32w=";
-    URI xsuaaBaseUri         = URI.create("https://d056076-sub1.authentication.eu10.hana.ondemand.com/");
-    URI tokenEndpointUri     = URI.create("https://d056076-sub1.authentication.eu10.hana.ondemand.com/oauth/token");
-    URI authorizeEndpointUri = URI.create("https://d056076-sub1.authentication.eu10.hana.ondemand.com/oauth/authorize");
-    URI keySetEndpointUri    = URI.create("https://d056076-sub1.authentication.eu10.hana.ondemand.com/token_keys");
+    URI xsuaaBaseUri         = URI.create("https://subdomain.authentication.eu10.hana.ondemand.com/");
+    URI tokenEndpointUri     = URI.create("https://subdomain.authentication.eu10.hana.ondemand.com/oauth/token");
 }
 //@formatter:on
