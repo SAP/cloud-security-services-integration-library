865a1d2ee18b8e4c6162004f5f05f0bbebdc6c12
RRoggia <renanzr@gmail.com>
2019-07-31 18:22:49 -0300

    remove unused logger imports

--- samples/spring-webflux-security-xsuaa-usage/src/main/java/sample/spring/webflux/xsuaa/TestController.java
+++ samples/spring-webflux-security-xsuaa-usage/src/main/java/sample/spring/webflux/xsuaa/TestController.java
@@ -1,13 +1,13 @@
 package sample.spring.webflux.xsuaa;
 
-import com.sap.cloud.security.xsuaa.token.ReactiveSecurityContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sap.cloud.security.xsuaa.token.ReactiveSecurityContext;
+
 import reactor.core.publisher.Mono;
 
 @RestController
