0ce4ee6e070bef78ae645bab1798547dd9837775
RRoggia <renanzr@gmail.com>
2019-08-01 10:41:18 -0300

    run tests with uaamock profile and start xsuaa mock server for the tests

---
+++ samples/spring-webflux-security-xsuaa-usage/src/test/java/sample/spring/webflux/xsuaa/mock/XsuaaMockPostProcessor.java
@@ -0,0 +1,26 @@
+package sample.spring.webflux.xsuaa.mock;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Profiles;
+
+import com.sap.cloud.security.xsuaa.mock.XsuaaMockWebServer;
+
+public class XsuaaMockPostProcessor implements EnvironmentPostProcessor, DisposableBean {
+
+	private final XsuaaMockWebServer mockAuthorizationServer = new XsuaaMockWebServer();
+
+	@Override
+	public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+		if (environment.acceptsProfiles(Profiles.of("uaamock"))) {
+			environment.getPropertySources().addFirst(this.mockAuthorizationServer);
+		}
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		this.mockAuthorizationServer.destroy();
+	}
+}
