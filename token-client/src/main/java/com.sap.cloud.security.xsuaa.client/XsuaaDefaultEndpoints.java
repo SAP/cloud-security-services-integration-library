fa57ef4d49ad4a6d4aeb7ecbe1d7e0e94f48dcf8
Nena Raab <nena.raab@sap.com>
2019-08-12 13:58:25 +0200

    introduce new token client lib

---
+++ token-client/src/main/java/com.sap.cloud.security.xsuaa.client/XsuaaDefaultEndpoints.java
@@ -0,0 +1,52 @@
+package com.sap.cloud.security.xsuaa.client;
+
+import org.springframework.util.Assert;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+public class XsuaaDefaultEndpoints implements OAuth2ServiceEndpointsProvider {
+	private URI baseUri;
+	private final static String TOKEN_ENDPOINT = "/oauth/token";
+	private final static String AUTHORIZE_ENDPOINT = "/oauth/authorize";
+	private final static String KEYSET_ENDPOINT = "/token_keys";
+
+	/**
+	 * Creates a new XsuaaRestClient.
+	 *
+	 * @param baseUri
+	 *            - the base URI of XSUAA. Based on the base URI the tokenEndpoint,
+	 *            authorize and key set URI (JWKS) will be derived.
+	 */
+	public XsuaaDefaultEndpoints(URI baseUri) {
+		Assert.notNull(baseUri, "XSUAA base URI must not be null.");
+		this.baseUri = baseUri;
+	}
+
+	/**
+	 * Creates a new XsuaaRestClient.
+	 *
+	 * @param baseUri
+	 *            - the base URI of XSUAA. Based on the base URI the tokenEndpoint,
+	 *            authorize and key set URI (JWKS) will be derived.
+	 */
+	public XsuaaDefaultEndpoints(String baseUri) {
+		Assert.notNull(baseUri, "XSUAA base URI must not be null.");
+		this.baseUri = URI.create(baseUri);
+	}
+
+	@Override
+	public URI getTokenEndpoint() {
+		return UriComponentsBuilder.fromUri(baseUri).path(TOKEN_ENDPOINT).build().toUri();
+	}
+
+	@Override
+	public URI getAuthorizeEndpoint() {
+		return UriComponentsBuilder.fromUri(baseUri).path(AUTHORIZE_ENDPOINT).build().toUri();
+	}
+
+	@Override
+	public URI getJwksUri() {
+		return UriComponentsBuilder.fromUri(baseUri).path(KEYSET_ENDPOINT).build().toUri();
+	}
+}
