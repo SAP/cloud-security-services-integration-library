44bc95c7e5452af60373e0984f278f3d38d674ea
Nena Raab <nena.raab@sap.com>
2019-08-26 12:27:08 +0200

    support uaa v74: refresh/clientcred should pass credentials via body - not auth header

--- token-client/src/main/java/com/sap/cloud/security/xsuaa/client/OAuth2TokenServiceConstants.java
+++ token-client/src/main/java/com/sap/cloud/security/xsuaa/client/OAuth2TokenServiceConstants.java
@@ -9,6 +9,8 @@ public class OAuth2TokenServiceConstants {
 	public static final String ACCESS_TOKEN = "access_token";
 	public static final String EXPIRES_IN = "expires_in";
 	public static final String REFRESH_TOKEN = "refresh_token";
+	public static final String CLIENT_ID = "client_id";
+	public static final String CLIENT_SECRET = "client_secret";
 
 	public static final String GRANT_TYPE = "grant_type";
 	public static final String GRANT_TYPE_USER_TOKEN = "user_token";
