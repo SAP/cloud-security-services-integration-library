7c203ca66868942dc6c44e42bb27d75be71d738a
Nena Raab <nena.raab@sap.com>
2019-09-16 16:39:57 +0200

    update spring sample with caffeine cache - Part 2

--- samples/spring-security-basic-auth/src/main/java/sample/spring/xsuaa/SecurityConfiguration.java
+++ samples/spring-security-basic-auth/src/main/java/sample/spring/xsuaa/SecurityConfiguration.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2002-2019 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package sample.spring.xsuaa;
 
 import java.util.concurrent.TimeUnit;
@@ -21,7 +6,6 @@ import com.github.benmanes.caffeine.cache.Caffeine;
 import com.sap.cloud.security.xsuaa.XsuaaServiceConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.Cache;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.convert.converter.Converter;
@@ -38,7 +22,6 @@ import com.sap.cloud.security.xsuaa.extractor.TokenBrokerResolver;
 import com.sap.cloud.security.xsuaa.token.TokenAuthenticationConverter;
 
 @EnableWebSecurity
-@EnableCaching
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -59,6 +42,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.bearerTokenResolver(getTokenBrokerResolver())
 				.jwt()
 				.jwtAuthenticationConverter(jwtAuthenticationConverter());
+
 		// @formatter:on
 	}
 
