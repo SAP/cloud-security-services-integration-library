4b7af818668bc2761a54f6f9c1c5ca9825b81fe7
nenaraab <nenaraab@users.noreply.github.com>
2019-10-15 00:57:12 +0200

    fix: MockWebServer allows to run at several ports in parallel (#153)

--- spring-xsuaa-mock/src/main/java/com/sap/cloud/security/xsuaa/mock/XsuaaMockWebServer.java
+++ spring-xsuaa-mock/src/main/java/com/sap/cloud/security/xsuaa/mock/XsuaaMockWebServer.java
@@ -1,18 +1,22 @@
 package com.sap.cloud.security.xsuaa.mock;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.core.env.PropertySource;
 import org.springframework.util.Assert;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import okhttp3.mockwebserver.Dispatcher;
 import okhttp3.mockwebserver.MockWebServer;
 
+/**
+ * Starts a mock for xsuaa (user account and authentication) service on localhost.
+ */
 public class XsuaaMockWebServer extends PropertySource<MockWebServer> implements DisposableBean {
 
 	public static final String MOCK_XSUAA_PROPERTY_SOURCE_NAME = "mockxsuaaserver";
@@ -21,9 +25,9 @@ public class XsuaaMockWebServer extends PropertySource<MockWebServer> implements
 	static final int MOCK_XSUAA_DEFAULT_PORT = 33195;
 	private final int port;
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(XsuaaMockWebServer.class);
+	private static final Logger log = LoggerFactory.getLogger(XsuaaMockWebServer.class);
 
-	private static Map<Integer, String> startedWebServer = new HashMap<>();
+	private static Map<Integer, String> startedWebServer = new ConcurrentHashMap<>();
 
 	public XsuaaMockWebServer() {
 		this(MOCK_XSUAA_DEFAULT_PORT);
@@ -34,6 +38,7 @@ public class XsuaaMockWebServer extends PropertySource<MockWebServer> implements
 	 *
 	 * @param port
 	 *            the port the mock server should listen to. Use '0' in case you want to use a random port.
+	 *            Per specified port you can only start one mock web server instance.
 	 */
 	public XsuaaMockWebServer(int port) {
 		super(MOCK_XSUAA_PROPERTY_SOURCE_NAME, createMockWebServer(new XsuaaRequestDispatcher()));
@@ -71,7 +76,7 @@ public class XsuaaMockWebServer extends PropertySource<MockWebServer> implements
 				intializeMockXsuaa(getSource(), port);
 			}
 			String url = startedWebServer.get(port);
-			LOGGER.info("return Mock Server url {} as property", url);
+			log.info("return Mock Server url {} as property", url);
 			return url;
 		} else {
 			return null;
@@ -81,26 +86,27 @@ public class XsuaaMockWebServer extends PropertySource<MockWebServer> implements
 	@Override
 	public void destroy() throws Exception {
 		getSource().shutdown();
-		LOGGER.info("shutdown Mock Server on port {} ", port);
+		log.info(">>>>>>>>>>> Stopped Xsuaa Mock Server (MockWebServer[{}]) ", port);
 		startedWebServer.remove(port);
-	}
-
-	private static String getUrlAndStartIfNotStarted(MockWebServer mockWebServer) {
-		String url = mockWebServer.url("").url().toExternalForm();
-		return url.substring(0, url.length() - 1).replace("127.0.0.1", "localhost");
 	}
 
 	private static void intializeMockXsuaa(MockWebServer mockWebServer, int port) {
 		try {
 			mockWebServer.start(port);
 			startedWebServer.put(port, getUrlAndStartIfNotStarted(mockWebServer));
-			LOGGER.warn(
-					">>>>>>>>>>>Started Xsuaa Mock Server ({}) that provides public keys for offline JWT Token validation. NEVER run in productive environment!<<<<<<", mockWebServer.url(""));
+			log.warn(
+					">>>>>>>>>>> Started Xsuaa Mock Server ({}) that provides public keys for offline JWT Token validation. NEVER run in productive environment!<<<<<<", mockWebServer.url(""));
 		} catch (IllegalStateException | IOException e) {
 			throw new IllegalStateException(
 					String.format("Could not start XSUAA Mock webserver (port:%d). " +
 							"Make sure that it is not yet started in another process.", port),
 					e);
 		}
+	}
+
+	private static String getUrlAndStartIfNotStarted(MockWebServer mockWebServer) {
+		String url = mockWebServer.url("").url().toExternalForm();
+		url = UriComponentsBuilder.fromHttpUrl(url).host("localhost").build().toUriString();
+		return url.substring(0, url.length() - 1); // removes trailing "/"
 	}
 }
