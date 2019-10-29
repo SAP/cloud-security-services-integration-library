a8f8a7e72910c34b16891dad55448a53e6c5651b
Nena Raab <nena.raab@sap.com>
2019-10-29 09:37:23 +0100

    TokenValidation without crypto lib

--- core/src/main/java/com/sap/cloud/security/token/validation/OAuth2TokenSignatureValidator.java
+++ core/src/main/java/com/sap/cloud/security/token/validation/OAuth2TokenSignatureValidator.java
@@ -1,82 +1,140 @@
 package com.sap.cloud.security.token.validation;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.nio.charset.StandardCharsets.*;
 
-import java.net.URI;
+import javax.annotation.Nullable;
+import javax.xml.bind.DatatypeConverter;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 import java.security.Signature;
+import java.security.SignatureException;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
-import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.sap.cloud.security.core.Assertions;
 import com.sap.cloud.security.core.config.OAuth2ServiceConfiguration;
+import com.sap.cloud.security.xsuaa.client.OAuth2ServiceEndpointsProvider;
 import com.sap.cloud.security.xsuaa.client.OAuth2ServiceException;
 import com.sap.cloud.security.xsuaa.client.OAuth2TokenKeyService;
-import com.sap.cloud.security.xsuaa.jwt.DecodedJwt;
+import com.sap.cloud.security.xsuaa.client.XsuaaDefaultEndpoints;
 import com.sap.cloud.security.xsuaa.jwt.JSONWebKey;
 import com.sap.cloud.security.xsuaa.jwt.JSONWebKeySet;
 
-public class OAuth2TokenSignatureValidator { //TODO implement interface
-	Map<String, PublicKey> keyCache = new HashMap<>();
-	OAuth2ServiceConfiguration serviceConfiguration;
-	OAuth2TokenKeyService tokenKeyService;
+public class : { //TODO implement interface
+	private Map<String, PublicKey> keyCache = new HashMap<>();
+	private OAuth2ServiceConfiguration serviceConfiguration;
+	private OAuth2TokenKeyService tokenKeyService;
+	private OAuth2ServiceEndpointsProvider tokenUrlProvider;
+	private static Logger LOGGER = LoggerFactory.getLogger(OAuth2TokenSignatureValidator.class);
 
 	public OAuth2TokenSignatureValidator(OAuth2ServiceConfiguration xsuaaServiceConfiguration, OAuth2TokenKeyService tokenKeyService) {
 		Assertions.assertNotNull(xsuaaServiceConfiguration, "'xsuaaServiceConfiguration' is required");
 		Assertions.assertNotNull(tokenKeyService, "'tokenKeyService' is required");
 		this.serviceConfiguration = xsuaaServiceConfiguration;
 		this.tokenKeyService = tokenKeyService;
+		this.tokenUrlProvider = new XsuaaDefaultEndpoints(serviceConfiguration.getUaaUrl());
 	}
 
-	public boolean validate(DecodedJwt token) { // TODO Decoded JWT
-		String kid = "key-id-1"; //TODO parse from JSON Header
-		String kty = "RS256"; //TODO parse from JSON Header
-		String jku = "https://authentication.stagingaws.hanavlab.ondemand.com/token_keys"; //TODO parse from JSON Header
+	public boolean validate(String token) {
+		// DecodedJwt decodedJwt = new Base64JwtDecoder().decode(token);
+		String kid = "key-id-1"; //TODO parse from JSON Header e.g. decodedJwt.getHeader().getKeyId()
+		String alg = "RS256"; //TODO parse from JSON Header e.g. decodedJwt.getHeader().getAlgorithm()
+		String jku = "https://authentication.stagingaws.hanavlab.ondemand.com/token_keys"; //TODO parse from JSON Header e.g. decodedJwt.getHeader().getJsonKeyUrl()
 
-		PublicKey publicKey = lookupCache(kid); // kid + alg?
 		// TODO validate jku by validating with uaaDomain
+
+		PublicKey publicKey = getPublicKey(kid, jku);
 		if (publicKey == null) {
-			try {
-				JSONWebKeySet jwks = tokenKeyService.retrieveTokenKeys(URI.create(jku));
-				publicKey = createPublicKey(jwks.getKeyByTypeAndId(JSONWebKey.Type.RSA, kid));
-			} catch (OAuth2ServiceException | NoSuchAlgorithmException | InvalidKeySpecException e) {
-				e.printStackTrace();
-				return false;
-			}
-		}
-		try {
-			return verify("RSA", token.getPayload(), token.getSignature(), publicKey);
-		} catch (Exception e) {
-			e.printStackTrace();
 			return false;
 		}
+		try {
+			return verifySignature(token, alg, publicKey);
+		} catch (Exception e) {
+			LOGGER.error("Error during JSON Web Signature could not be verified.", e);
+		}
+		return false;
+	}
 
+	@Nullable
+	private PublicKey getPublicKey(String kid, String jku) {
+		PublicKey publicKey = lookupCache(kid); // TODO kid + kty?
+		if (publicKey == null) {
+			try {
+				JSONWebKeySet jwks = tokenKeyService.retrieveTokenKeys(tokenUrlProvider.getAuthorizeEndpoint()); // TODO
+				JSONWebKey jwk = jwks.getKeyByTypeAndId(JSONWebKey.Type.RSA, kid); // TODO check
+				publicKey = createPublicKeyFromString(jwk.getType(), jwk.getPublicKey());
+				keyCache.put(kid, publicKey); //TODO kid + kty
+			} catch (OAuth2ServiceException e) {
+				LOGGER.error("Error retrieving token keys from Identity Service ({}).", tokenUrlProvider.getAuthorizeEndpoint(), e);
+			} catch (NoSuchAlgorithmException | UnsupportedEncodingException | InvalidKeySpecException e) {
+				LOGGER.error("Error creating PublicKey from JSON Web Key received from Identity Service({}).", tokenUrlProvider.getAuthorizeEndpoint(), e);
+			}
+		}
+		return publicKey;
 	}
 
 	private PublicKey lookupCache(String kid) {
 		return keyCache.get(kid);
 	}
 
-	private PublicKey createPublicKey(JSONWebKey webKey) throws NoSuchAlgorithmException, InvalidKeySpecException {
-			KeyFactory keyFactory = KeyFactory.getInstance(webKey.getType().value()); // "RSA"
-
-			X509EncodedKeySpec keySpecX509 = new X509EncodedKeySpec(Base64.getDecoder().decode(webKey.getPublicKey()));
-			return keyFactory.generatePublic(keySpecX509);
+	private PublicKey createPublicKeyFromString(JSONWebKey.Type webKeyType, String publicKey)
+			throws NoSuchAlgorithmException, InvalidKeySpecException, UnsupportedEncodingException {
+		if(webKeyType != JSONWebKey.Type.RSA) {
+			throw new IllegalStateException("JWT token with web key type " + webKeyType + " can not be verified.");
 		}
+		KeyFactory keyFactory = KeyFactory.getInstance(webKeyType.value()); // "RSA"
+
+		byte[] keyBytes = DatatypeConverter.parseBase64Binary(
+			new String(publicKey.getBytes(UTF_8.name()), UTF_8));
+
+		X509EncodedKeySpec keySpecX509 = new X509EncodedKeySpec(keyBytes);
+		return keyFactory.generatePublic(keySpecX509);
+	}
 
 
-	private static boolean verify(String algorithm, String plainText, String signature, PublicKey publicKey) throws Exception {
-		Signature publicSignature = Signature.getInstance("SHA256withRSA"); //"SHA256withRSA"
+	private static boolean verifySignature(String token, String algorithm, PublicKey publicKey) throws
+			SignatureException, InvalidKeyException, NoSuchAlgorithmException, UnsupportedEncodingException {
+		if(!"RS256".equalsIgnoreCase(algorithm)) {
+			throw new IllegalStateException("JWT token with signature algorithm " + algorithm + " can not be verified.");
+		}
+		String[] tokenHeaderPayloadSignature = token.split(Pattern.quote("."));
+		if(tokenHeaderPayloadSignature.length != 3) {
+			throw new IllegalArgumentException("JWT token does not consist of 'header'.'payload'.'signature'.");
+		}
+		String headerAndPayload = new StringBuilder( tokenHeaderPayloadSignature[0]).append( "." ).append(tokenHeaderPayloadSignature[1]).toString();
+
+		Signature publicSignature = Signature.getInstance("SHA256withRSA"); //RSASSA-PKCS1-v1_5 using SHA-256 according to https://tools.ietf.org/html/rfc7518#section-3
 		publicSignature.initVerify(publicKey);
-		publicSignature.update(plainText.getBytes(UTF_8));
+		publicSignature.update(headerAndPayload.getBytes( UTF_8)); // provide data
 
-		byte[] signatureBytes = Base64.getDecoder().decode(signature);
+		boolean isSignatureValid = publicSignature.verify(parseTokenSignature(tokenHeaderPayloadSignature[2]));
+		if(!isSignatureValid) {
+			LOGGER.error("Signature of JWT Token is not valid: the identity provided by the JSON Web Token Key can not be verified");
+		}
+		return isSignatureValid;
+	}
 
-		return publicSignature.verify(signatureBytes);
+	private static byte[] parseTokenSignature(final String signature) throws UnsupportedEncodingException {
+		String tokenSignature = signature;
+		int padding = 4 - tokenSignature.getBytes(UTF_8.name()).length % 4;
+		if (padding != 4) {
+			for (int i = 0; i < padding; ++i)
+				tokenSignature += '=';
+		}
+		tokenSignature = tokenSignature.replaceAll("-", "+");
+		tokenSignature = tokenSignature.replaceAll("_", "/");
+
+		return DatatypeConverter.parseBase64Binary(
+				new String(tokenSignature.getBytes(UTF_8.name()), UTF_8));
 	}
 }
--- core/src/test/java/com/sap/cloud/security/token/validation/OAuth2TokenSignatureValidatorTest.java
+++ core/src/test/java/com/sap/cloud/security/token/validation/OAuth2TokenSignatureValidatorTest.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.when;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
+import java.util.regex.Pattern;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.Before;
@@ -16,36 +17,35 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import com.sap.cloud.security.core.config.OAuth2ServiceConfiguration;
+import com.sap.cloud.security.xsuaa.client.OAuth2ServiceException;
 import com.sap.cloud.security.xsuaa.client.OAuth2TokenKeyService;
-import com.sap.cloud.security.xsuaa.jwt.Base64JwtDecoder;
-import com.sap.cloud.security.xsuaa.jwt.DecodedJwt;
 import com.sap.cloud.security.xsuaa.jwt.JSONWebKey;
 import com.sap.cloud.security.xsuaa.jwt.JSONWebKeyImpl;
 import com.sap.cloud.security.xsuaa.jwt.JSONWebKeySet;
 
-@Ignore
 public class OAuth2TokenSignatureValidatorTest {
 	private String pemEncodedPublicKey = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAmNM3OXfZS0Uu8eYZXCgG\nWFgVWQX6MHnadYk3zHZ5PIPeDY3ApaSGpMEiVwn1cT6KUVHMLHcmz1gsNy74pCnm\nCa22W7J3BoZulzR0A37ayMVrRHh3nffgySk8u581l02bvbcpab3e3EotSN5LixGT\n1VWZvDbalXf6n5kq459NWL6ZzkEs20MrOEk5cLdnsigTQUHSKIQ5TpldyDkJMm2z\nwOlrB2R984+QdlDFmoVH7Yaujxr2g0Z96u7W9Imik6wTiFc8W7eUXfhPGn7reVLq\n1/5o2Nz0Jx0ejFHDwTGncs+k1RBS6DbpTOMr9tkJEc3ZsX3Ft4OtqCkRXI5hUma+\nHwIDAQAB\n-----END PUBLIC KEY-----";
 	private String accessToken;
+	private String otherToken; // contains alg header only, signature that does not match jwks
 	private OAuth2TokenSignatureValidator cut;
 	private OAuth2TokenKeyService serviceMock;
-	private OAuth2ServiceConfiguration serviceConfiguration;
+	private OAuth2ServiceConfiguration serviceConfigurationMock;
 
 	@Before
 	public void setup() throws IOException {
 		accessToken = IOUtils.resourceToString("/xsuaaAccessTokenRSA256.txt", StandardCharsets.UTF_8);
+		otherToken = IOUtils.resourceToString("/iasOIDCTokenAlgHeaderOnly.txt", StandardCharsets.UTF_8);
 
-		serviceConfiguration = Mockito.mock(OAuth2ServiceConfiguration.class);
-		when(serviceConfiguration.getUaaUrl()).thenReturn(URI.create("https://subdomain.myauth.com"));
+		serviceConfigurationMock = Mockito.mock(OAuth2ServiceConfiguration.class);
+		when(serviceConfigurationMock.getUaaUrl()).thenReturn(URI.create("https://subdomain.myauth.com"));
 
 		serviceMock = Mockito.mock(OAuth2TokenKeyService.class);
 		when(serviceMock.retrieveTokenKeys(any())).thenReturn(createJSONWebKeySet());
 
-		cut = new OAuth2TokenSignatureValidator(serviceConfiguration, serviceMock);
+		cut = new OAuth2TokenSignatureValidator(serviceConfigurationMock, serviceMock);
 	}
 
 	private JSONWebKeySet createJSONWebKeySet() {
-		//TODO PublicKey publicKey =
 		JSONWebKey jsonWebKey = new JSONWebKeyImpl(
 				JSONWebKey.Type.RSA, "key-id-1", "RS256", pemEncodedPublicKey);
 		JSONWebKeySet keySet = new JSONWebKeySet();
@@ -54,8 +54,87 @@ public class OAuth2TokenSignatureValidatorTest {
 	}
 
 	@Test
-	public void checkAccessTokenIsCorrect() {
-		DecodedJwt jwt = new Base64JwtDecoder().decode(accessToken);
-		assertThat(cut.validate(jwt), is(true));
+	public void jsonWebSignatureMatchesJWKS() throws IOException {
+		assertThat(cut.validate(accessToken), is(true));
+	}
+
+	@Test
+	public void jsonWebSignatureDoesNotMatchJWKS() {
+		String[] tokenHeaderPayloadSignature = accessToken.split(Pattern.quote("."));
+		String[] otherHeaderPayloadSignature = otherToken.split(Pattern.quote("."));
+		String tokenWithOthersSignature = new StringBuilder(tokenHeaderPayloadSignature[0])
+								.append(".")
+								.append(tokenHeaderPayloadSignature[1])
+								.append(".")
+								.append(otherHeaderPayloadSignature[2]).toString();
+		assertThat(cut.validate(tokenWithOthersSignature), is(false));
+	}
+
+	@Test
+	public void jwtPayloadModifiedNotValid() {
+		String[] tokenHeaderPayloadSignature = accessToken.split(Pattern.quote("."));
+		String[] otherHeaderPayloadSignature = otherToken.split(Pattern.quote("."));
+		String tokenWithOthersSignature = new StringBuilder(tokenHeaderPayloadSignature[0])
+				.append(".")
+				.append(otherHeaderPayloadSignature[1])
+				.append(".")
+				.append(tokenHeaderPayloadSignature[2]).toString();
+		assertThat(cut.validate(tokenWithOthersSignature), is(false));
+	}
+
+	@Test
+	public void jwtHeaderModifiedNotValid() {
+		String[] tokenHeaderPayloadSignature = accessToken.split(Pattern.quote("."));
+		String[] otherHeaderPayloadSignature = otherToken.split(Pattern.quote("."));
+		String tokenWithOthersSignature = new StringBuilder(otherHeaderPayloadSignature[0])
+				.append(".")
+				.append(tokenHeaderPayloadSignature[1])
+				.append(".")
+				.append(tokenHeaderPayloadSignature[2]).toString();
+		assertThat(cut.validate(tokenWithOthersSignature), is(false));
+	}
+
+	@Test
+	public void jwtWithAlgHeaderOnlyNotValid() {
+		assertThat(cut.validate(otherToken), is(false));
+	}
+
+	@Test
+	public void jwtWithoutSignatureNotValid() {
+		String[] tokenHeaderPayloadSignature = accessToken.split(Pattern.quote("."));
+		String tokenWithOthersSignature = new StringBuilder(tokenHeaderPayloadSignature[0])
+				.append(".")
+				.append(tokenHeaderPayloadSignature[1]).toString();
+		assertThat(cut.validate(tokenWithOthersSignature), is(false));
+	}
+
+	@Test
+	@Ignore
+	public void takePublicKeyFromCache() {
+		// TODO implement
+	}
+
+	@Test
+	public void validationFailsWhenTokenKeyCanNotBeRetrievedFromIdentityProvider() throws OAuth2ServiceException {
+		when(serviceMock.retrieveTokenKeys(any())).thenThrow(new OAuth2ServiceException("Currently unavailable"));
+		assertThat(cut.validate(accessToken), is(false));
+	}
+
+	@Test
+	@Ignore
+	public void validationFailsWhenTokenKeyUrlDoesNotMatchIdentityProviderDomain() {
+		// TODO implement
+	}
+
+	@Test
+	@Ignore
+	public void requiredParameters() {
+		// TODO implement
+	}
+
+	@Test
+	@Ignore
+	public void webTokenKeyOtherThanRSAIsNotSupported() {
+		// TODO implement
 	}
 }
