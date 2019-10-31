085dc20c1c0c8eee10a168c9c0c3153f95e918a5
Daniel Hassler <daniel.hassler@sap.com>
2019-10-31 14:06:10 +0100

    Added documentation to ValidationResults

--- core/src/main/java/com/sap/cloud/security/token/validation/ValidationResults.java
+++ core/src/main/java/com/sap/cloud/security/token/validation/ValidationResults.java
@@ -2,15 +2,33 @@ package com.sap.cloud.security.token.validation;
 
 import java.util.ArrayList;
 
+/**
+ * This is a factory class to easily create {@link ValidationResult} objects.
+ */
 public class ValidationResults {
 
-	public static ValidationResult createInvalid(String errorMessage) {
+	/**
+	 * Creates an invalid {@link ValidationResult} that contains one
+	 * {@link ValidationError} with given description.
+	 * 
+	 * @param errorDescription
+	 *            the description used to create the {@link ValidationError}.
+	 * @return an invalid {@link ValidationResult} containing one
+	 *         {@link ValidationError} with the given error description.
+	 */
+	public static ValidationResult createInvalid(String errorDescription) {
 		ArrayList<ValidationError> validationErrors = new ArrayList<>();
-		validationErrors.add(new ValidationErrorImpl(errorMessage));
+		validationErrors.add(new ValidationErrorImpl(errorDescription));
 		ValidationResultImpl validationResult = new ValidationResultImpl(validationErrors);
 		return validationResult;
 	}
 
+	/**
+	 * Creates a valid {@link ValidationResult}, which is a {@link ValidationResult}
+	 * that contains no errors.
+	 * 
+	 * @return a valid validation result.
+	 */
 	public static ValidationResult createValid() {
 		return new ValidationResultImpl(new ArrayList<>());
 	}
