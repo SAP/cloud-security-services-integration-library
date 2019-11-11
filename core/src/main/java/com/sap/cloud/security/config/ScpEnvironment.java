41c77f9992eed4bf338567349ce16d54aaedcef3
Nena Raab <nena.raab@sap.com>
2019-11-11 12:48:55 +0100

    comments ScpEnvironement

--- core/src/main/java/com/sap/cloud/security/config/ScpEnvironment.java
+++ core/src/main/java/com/sap/cloud/security/config/ScpEnvironment.java
@@ -1,13 +1,18 @@
 package com.sap.cloud.security.config;
 
-
+// TODO rename Environment
 public class ScpEnvironment {
 
 	private ScpEnvironment() {
 
 	}
 
+	// TODO getInstance() -> change to non-static
+	// TODO check whether env needs to be specified or whether we determine it internally
 	public static OAuth2ServiceConfiguration getXsuaaServiceConfiguration(String envName) {
 		return null;
 	}
+
+	// TODO
+	// setXsuaaServiceConfiguration(String resourceName...???)
 }
