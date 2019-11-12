3cfba8edfe27c685303989aa69abf533b71dd7be
Daniel Hassler <daniel.hassler@sap.com>
2019-11-12 14:13:10 +0100

    implemented unit test TokenFilterTest

--- samples/java-security-usage/src/main/java/com/sap/cloud/security/javasec/samples/usage/TokenFilter.java
+++ samples/java-security-usage/src/main/java/com/sap/cloud/security/javasec/samples/usage/TokenFilter.java
@@ -5,6 +5,7 @@ import com.sap.cloud.security.token.SecurityContext;
 import com.sap.cloud.security.token.Token;
 import com.sap.cloud.security.token.TokenImpl;
 import com.sap.cloud.security.token.validation.ValidationResult;
+import com.sap.cloud.security.token.validation.Validator;
 import com.sap.cloud.security.token.validation.validators.CombiningValidator;
 import com.sap.cloud.security.xsuaa.http.HttpHeaders;
 import org.slf4j.Logger;
@@ -17,15 +18,30 @@ import javax.servlet.http.HttpServletResponse;
 public class TokenFilter implements Filter {
 
 	private static final Logger logger = LoggerFactory.getLogger(TokenFilter.class);
+	private final TokenExtractor tokenExtractor;
+	private final Validator<Token> tokenValidator;
+	
+	
+	interface TokenExtractor {
+		Token fromAuthorizationHeader(String authorizationHeader);
+	}
+	
+	public TokenFilter() {
+		tokenExtractor = (authorizationHeader) -> new TokenImpl(authorizationHeader);
+		tokenValidator = CombiningValidator.builderFor(Environment.getInstance().getXsuaaServiceConfiguration()).build();
+	}
+
+	TokenFilter(TokenExtractor tokenExtractor, Validator<Token> tokenValidator) {
+		this.tokenExtractor = tokenExtractor;
+		this.tokenValidator = tokenValidator;
+	}
 
 	@Override
 	public void init(FilterConfig filterConfig) {
 	}
-
+	
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response,
-			FilterChain filterChain) {
-
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)  {
 		if (request instanceof HttpServletRequest && response instanceof HttpServletResponse) {
 			HttpServletRequest httpRequest = (HttpServletRequest) request;
 			HttpServletResponse httpResponse = (HttpServletResponse) response;
@@ -33,8 +49,8 @@ public class TokenFilter implements Filter {
 
 			if (headerIsAvailable(authorizationHeader)) {
 				try {
-					Token token = new TokenImpl(authorizationHeader);
-					ValidationResult result = validateToken(token);
+					Token token = tokenExtractor.fromAuthorizationHeader(authorizationHeader);
+					ValidationResult result = tokenValidator.validate(token);
 					if (result.isValid()) {
 						SecurityContext.setToken(token);
 						filterChain.doFilter(request, response);
@@ -42,19 +58,12 @@ public class TokenFilter implements Filter {
 						unauthorized(httpResponse, "Error during token validation: " + result.getErrorDescription());
 					}
 				} catch (Exception e) {
-					unauthorized(httpResponse, "Unexpected Error occured: " + e.getMessage());
+					unauthorized(httpResponse, "Unexpected error occured: " + e.getMessage());
 				}
 			} else {
 				unauthorized(httpResponse, "Authorization header is missing");
 			}
 		}
-	}
-
-	private ValidationResult validateToken(Token token) {
-		CombiningValidator<Token> combiningValidator =
-				CombiningValidator.builderFor(Environment.getInstance().getXsuaaServiceConfiguration())
-						.build();// NEEDS VCAP_SERVICES env
-		return combiningValidator.validate(token);
 	}
 
 	private void unauthorized(HttpServletResponse httpResponse, String message) {
---
+++ samples/java-security-usage/src/test/java/com/sap/cloud/security/javasec/samples/usage/TokenFilterTest.java
@@ -0,0 +1,96 @@
+package com.sap.cloud.security.javasec.samples.usage;
+
+import com.sap.cloud.security.token.SecurityContext;
+import com.sap.cloud.security.token.Token;
+import com.sap.cloud.security.token.validation.ValidationResult;
+import com.sap.cloud.security.token.validation.ValidationResults;
+import com.sap.cloud.security.xsuaa.http.HttpHeaders;
+import org.apache.http.HttpStatus;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+public class TokenFilterTest {
+
+	public static final Token TOKEN = Mockito.mock(Token.class);
+	private TokenFilter cut = null;
+	private HttpServletResponse httpResponse;
+	private HttpServletRequest httpRequest;
+	private FilterChain filterChain;
+
+	@Before
+	public void setUp() {
+		httpRequest = Mockito.mock(HttpServletRequest.class);
+		httpResponse = Mockito.mock(HttpServletResponse.class);
+		filterChain = Mockito.mock(FilterChain.class);
+		when(httpRequest.getHeader(HttpHeaders.AUTHORIZATION)).thenReturn("Bearer fake token");
+		cut = createComponent(ValidationResults.createValid());
+	}
+
+	@Test
+	public void doFilter_noHeader_isUnauthorized()  {
+		when(httpRequest.getHeader(HttpHeaders.AUTHORIZATION)).thenReturn(null);
+
+		cut.doFilter(httpRequest, httpResponse, filterChain);
+
+		assertThatResponseIsUnauthorized();
+	}
+
+	@Test
+	public void doFilter_invalidToken_isUnauthorized()  {
+		cut = createComponent((ValidationResults.createInvalid("Token is not valid")));
+
+		cut.doFilter(httpRequest, httpResponse, filterChain);
+
+		assertThatResponseIsUnauthorized();
+	}
+
+	@Test
+	public void doFilter_validToken_isNotUnauthorized()  {
+		cut = createComponent((ValidationResults.createValid()));
+
+		cut.doFilter(httpRequest, httpResponse, filterChain);
+
+		Mockito.verifyNoInteractions(httpResponse);
+	}
+
+	@Test
+	public void doFilter_validToken_containedInSecurityContext()  {
+		cut = createComponent((ValidationResults.createValid()));
+
+		cut.doFilter(httpRequest, httpResponse, filterChain);
+
+		assertThat(SecurityContext.getToken()).isSameAs(TOKEN);
+	}
+
+	@Test
+	public void doFilter_validToken_filterChainIsCalled() throws IOException, ServletException {
+		cut = createComponent((ValidationResults.createValid()));
+
+		cut.doFilter(httpRequest, httpResponse, filterChain);
+
+		Mockito.verify(filterChain, times(1)).doFilter(httpRequest, httpResponse);
+	}
+
+	private TokenFilter createComponent(ValidationResult validationResult) {
+		return new TokenFilter((header) -> TOKEN, (TOKEN) -> validationResult);
+	}
+
+	private void assertThatResponseIsUnauthorized() {
+		Mockito.verify(httpResponse, times(1)).setStatus(HttpStatus.SC_UNAUTHORIZED);
+		Mockito.verifyNoMoreInteractions(httpResponse);
+	}
+
+}
