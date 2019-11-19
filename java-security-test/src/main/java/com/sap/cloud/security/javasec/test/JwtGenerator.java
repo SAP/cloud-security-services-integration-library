6e9378f9050cd0ff3a2476e07bfeb71e424a37eb
Daniel Hassler <daniel.hassler@sap.com>
2019-11-19 16:12:30 +0100

    not exposing exceptions that are not dependend on user input

--- java-security-test/src/main/java/com/sap/cloud/security/javasec/test/JwtGenerator.java
+++ java-security-test/src/main/java/com/sap/cloud/security/javasec/test/JwtGenerator.java
@@ -3,15 +3,15 @@ package com.sap.cloud.security.javasec.test;
 import com.sap.cloud.security.token.Token;
 import com.sap.cloud.security.token.TokenImpl;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.SignatureException;
+import java.security.*;
 import java.util.Base64;
 
 // TODO 14.11.19 c5295400: This is basically a TokenBuilder?
 public class JwtGenerator {
+	private static final Logger logger = LoggerFactory.getLogger(JwtGenerator.class);
 
 	private static final String JWT_HEADER_ALG = "alg";
 	private static final String DOT = ".";
@@ -19,10 +19,10 @@ public class JwtGenerator {
 	private final JSONObject jsonHeader = new JSONObject();
 	private final JSONObject jsonPayload = new JSONObject();
 
-	private JwtSignatureAlgoritm jwtSignatureAlgoritm;
+	private JwtSignatureAlgoritm signatureAlgorithm;
 
 	public JwtGenerator() {
-		jwtSignatureAlgoritm = JwtSignatureAlgoritm.RS256;
+		signatureAlgorithm = JwtSignatureAlgoritm.RS256;
 	}
 
 	public JwtGenerator withHeaderParameter(String parameterName, String value) {
@@ -35,13 +35,12 @@ public class JwtGenerator {
 		return this;
 	}
 
-	public JwtGenerator withSignatureAlgoritm(JwtSignatureAlgoritm jwtSignatureAlgoritm) {
-		this.jwtSignatureAlgoritm = jwtSignatureAlgoritm;
+	public JwtGenerator withSignatureAlgoritm(JwtSignatureAlgoritm signatureAlgorithm) {
+		this.signatureAlgorithm = signatureAlgorithm;
 		return this;
 	}
 
-	public Token createToken(PrivateKey privateKey)
-			throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+	public Token createToken(PrivateKey privateKey) throws InvalidKeyException {
 		setHeaderAlgorithmValue();
 		String header = base64Encode(jsonHeader.toString().getBytes());
 		String payload = base64Encode(jsonPayload.toString().getBytes());
@@ -51,16 +50,22 @@ public class JwtGenerator {
 	}
 
 	private void setHeaderAlgorithmValue() {
-		withHeaderParameter(JWT_HEADER_ALG, jwtSignatureAlgoritm.getJwtAlgorithmHeaderValue());
+		withHeaderParameter(JWT_HEADER_ALG, signatureAlgorithm.getJwtAlgorithmHeaderValue());
 	}
 
-	private byte[] calculateSignature(byte[] headerAndPayload, PrivateKey privateKey)
-			throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
-		java.security.Signature signature = java.security.Signature
-				.getInstance(this.jwtSignatureAlgoritm.getJavaSignatureAlgorithmName());
-		signature.initSign(privateKey);
-		signature.update(headerAndPayload);
-		return signature.sign();
+	private byte[] calculateSignature(byte[] headerAndPayload, PrivateKey privateKey) throws InvalidKeyException {
+		try {
+			Signature signature = Signature.getInstance(signatureAlgorithm.getJavaSignatureAlgorithmName());
+			signature.initSign(privateKey);
+			signature.update(headerAndPayload);
+			return signature.sign();
+		} catch (NoSuchAlgorithmException e) {
+			logger.error("Algorithm '{}' not found!", signatureAlgorithm.getJavaSignatureAlgorithmName(), e);
+			throw new RuntimeException(e);
+		} catch (SignatureException e) {
+			logger.error("Error creating JWT signature!", e);
+			throw new RuntimeException(e);
+		}
 	}
 
 	private String base64Encode(byte[] bytes) {
