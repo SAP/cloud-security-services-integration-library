58af9bc1f510f97ce21af54ab181ed8297029031
Nena Raab <nena.raab@sap.com>
2019-11-29 17:09:00 +0100

    simplify /SecurityIntegrationTestRuleTest

--- java-security-test/src/test/java/com/sap/cloud/security/test/SecurityIntegrationTestRuleTest.java
+++ java-security-test/src/test/java/com/sap/cloud/security/test/SecurityIntegrationTestRuleTest.java
@@ -1,11 +1,14 @@
 package com.sap.cloud.security.test;
 
 import com.sap.cloud.security.config.Service;
+import com.sap.cloud.security.token.Token;
 import com.sap.cloud.security.token.TokenClaims;
-import com.sap.cloud.security.xsuaa.jwk.JsonWebKeyConstants;
+import com.sap.cloud.security.token.TokenHeader;
+import com.sap.cloud.security.xsuaa.jwk.JsonWebKeySet;
+import com.sap.cloud.security.xsuaa.jwk.JsonWebKeySetFactory;
+import com.sap.cloud.security.xsuaa.jwt.JwtSignatureAlgorithm;
+
 import org.apache.commons.io.IOUtils;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -16,7 +19,6 @@ import wiremock.org.apache.http.impl.client.HttpClients;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.Base64;
 import java.util.stream.Collectors;
 
 import static com.sap.cloud.security.config.Service.XSUAA;
@@ -31,43 +33,48 @@ public class SecurityIntegrationTestRuleTest {
 	private static final RSAKeys RSA_KEYS = RSAKeys.generate();
 
 	@ClassRule
-	public static SecurityIntegrationTestRule cut = SecurityIntegrationTestRule.getInstance(XSUAA)
+	public static SecurityIntegrationTestRule rule = SecurityIntegrationTestRule.getInstance(XSUAA)
 			.setPort(PORT)
 			.setKeys(RSA_KEYS)
 			.useApplicationServer("src/test/webapp", APPLICATION_SERVER_PORT);
 
-	public SecurityIntegrationTestRuleTest() {
-		cut.getPreconfiguredJwtGenerator().withHeaderParameter("header", "h-value");
-	}
-
 	@Test
 	public void getTokenKeysRequest_responseContainsExpectedTokenKeys() throws IOException {
 		HttpGet httpGet = new HttpGet("http://localhost:" + PORT + "/token_keys");
+
 		try (CloseableHttpResponse response = HttpClients.createDefault().execute(httpGet)) {
 			assertThat(response.getStatusLine().getStatusCode()).isEqualTo(HttpStatus.SC_OK);
-			String content = readContent(response);
-			JSONArray tokenKeys = new JSONObject(content).getJSONArray(JsonWebKeyConstants.KEYS_PARAMETER_NAME);
-			assertThat(tokenKeys).hasSize(1);
-			assertThat(tokenKeys.get(0)).isInstanceOf(JSONObject.class);
-			JSONObject tokenKeyObject = (JSONObject) tokenKeys.get(0);
-			String encodedPublicKey = Base64.getEncoder().withoutPadding()
-					.encodeToString(RSA_KEYS.getPublic().getEncoded());
-			assertThat(tokenKeyObject.get(JsonWebKeyConstants.VALUE_PARAMETER_NAME)).isEqualTo(encodedPublicKey);
+			JsonWebKeySet keySet = JsonWebKeySetFactory.createFromJson(readContent(response));
+			assertThat(keySet.getKeyByAlgorithmAndId(JwtSignatureAlgorithm.RS256, "default-kid")).isNotNull();
 		}
 	}
 
 	@Test
-	public void withClientId() {
-		assertThat(cut.setClientId("customClientId").createToken().getClaimAsString(TokenClaims.XSUAA.CLIENT_ID))
+	public void generatesTokenWithClientId() {
+		Token generatedToken = rule.setClientId("customClientId").createToken();
+		assertThat(generatedToken.getClaimAsString(TokenClaims.XSUAA.CLIENT_ID))
 				.isEqualTo("customClientId");
 	}
 
 	@Test
+	public void generatesTokenWithOtherClaimsAndHeaderParameter() {
+		Token generatedToken = rule.setClientId("customClientId").getPreconfiguredJwtGenerator()
+				.withClaimValue(TokenClaims.ISSUER, "issuer")
+				.withScopes("appid.scope1")
+				.withHeaderParameter(TokenHeader.TYPE, "type").createToken();
+
+		assertThat(generatedToken.getClaimAsString(TokenClaims.XSUAA.CLIENT_ID)).isEqualTo("customClientId");
+		assertThat(generatedToken.getClaimAsStringList(TokenClaims.XSUAA.SCOPES).size()).isEqualTo(1);
+		assertThat(generatedToken.getClaimAsString(TokenClaims.ISSUER)).isEqualTo("issuer");
+		assertThat(generatedToken.getHeaderParameterAsString(TokenHeader.TYPE)).isEqualTo("type");
+	}
+
+	@Test
 	public void testRuleIsInitializedCorrectly() {
-		assertThat(cut.getAppServerUri()).isEqualTo("http://localhost:" + APPLICATION_SERVER_PORT);
-		assertThat(cut.getWireMockRule()).isNotNull();
-		assertThat(cut.createToken().getAccessToken())
-				.isEqualTo(cut.getPreconfiguredJwtGenerator().createToken().getAccessToken());
+		assertThat(rule.getAppServerUri()).isEqualTo("http://localhost:" + APPLICATION_SERVER_PORT);
+		assertThat(rule.getWireMockRule()).isNotNull();
+		assertThat(rule.createToken().getAccessToken())
+				.isEqualTo(rule.getPreconfiguredJwtGenerator().createToken().getAccessToken());
 	}
 
 	private String readContent(CloseableHttpResponse response) throws IOException {
@@ -75,7 +82,7 @@ public class SecurityIntegrationTestRuleTest {
 				.collect(Collectors.joining());
 	}
 
-	public static class SecurityIntegrationTestRuleTestWithouthApplicationServer {
+	public static class SecurityIntegrationTestRuleTestWithoutApplicationServer {
 
 		@Rule
 		public SecurityIntegrationTestRule rule = SecurityIntegrationTestRule.getInstance(XSUAA);
