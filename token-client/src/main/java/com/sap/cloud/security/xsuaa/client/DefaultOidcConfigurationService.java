f27f265f6c9eccb66164438b35e211889557edc4
Nena Raab <nena.raab@sap.com>
2019-12-03 19:07:19 +0100

    token-client supports /.well-known/openid-configuration

---
+++ token-client/src/main/java/com/sap/cloud/security/xsuaa/client/DefaultOidcConfigurationService.java
@@ -0,0 +1,83 @@
+package com.sap.cloud.security.xsuaa.client;
+
+import javax.annotation.Nonnull;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sap.cloud.security.xsuaa.Assertions;
+import com.sap.cloud.security.xsuaa.util.HttpClientUtil;
+import com.sap.cloud.security.xsuaa.util.UriUtil;
+
+public class DefaultOidcConfigurationService implements OidcConfigurationService{
+
+	static final Logger logger = LoggerFactory.getLogger(DefaultOAuth2TokenService.class);
+	private final CloseableHttpClient httpClient;
+
+
+	public DefaultOidcConfigurationService(CloseableHttpClient httpClient) {
+		Assertions.assertNotNull(httpClient, "httpClient is required");
+		this.httpClient = httpClient;
+	}
+
+	@Override public OAuth2ServiceEndpointsProvider retrieveIssuerEndpoints(@Nonnull URI issuerUri)
+			throws OAuth2ServiceException {
+		URI discoveryEndpointUri = UriUtil.getUriWithPathAppended(issuerUri, DISCOVERY_ENDPOINT_DEFAULT);
+		return retrieveEndpoints(discoveryEndpointUri);
+	}
+
+	@Override public OAuth2ServiceEndpointsProvider retrieveEndpoints(@Nonnull URI discoveryEndpointUri)
+			throws OAuth2ServiceException {
+		Assertions.assertNotNull(discoveryEndpointUri, "discoveryEndpointUri must not be null!");
+		HttpUriRequest request = new HttpGet(discoveryEndpointUri);
+		try (CloseableHttpResponse response = httpClient.execute(request)) {
+			String bodyAsString = HttpClientUtil.extractResponseBodyAsString(response);
+			int statusCode = response.getStatusLine().getStatusCode();
+			return handleResponse(bodyAsString, statusCode);
+		} catch (IOException e) {
+			throw new OAuth2ServiceException("Error retrieving configured oidc endpoints from " + discoveryEndpointUri + " : " + e.getMessage());
+		}
+	}
+
+	private OAuth2ServiceEndpointsProvider handleResponse(String bodyAsString, int statusCode) throws OAuth2ServiceException {
+		if (statusCode == HttpStatus.SC_OK) {
+			return new OidcEndpointsProvider(bodyAsString);
+		} else {
+			throw OAuth2ServiceException
+					.createWithStatusCodeAndResponseBody("Error retrieving configured oidc endpoints", statusCode, bodyAsString);
+		}
+	}
+
+	static class OidcEndpointsProvider implements OAuth2ServiceEndpointsProvider {
+		final String AUTHORIZATION_ENDPOINT = "authorization_endpoint";
+		final String TOKEN_ENDPOINT = "token_endpoint";
+		final String JWKS_ENDPOINT = "jwks_uri";
+
+		private JSONObject jsonObject;
+
+		OidcEndpointsProvider(String jsonString) {
+			jsonObject = new JSONObject(jsonString);
+		}
+
+		@Override public URI getTokenEndpoint() {
+			return URI.create(jsonObject.getString(TOKEN_ENDPOINT));
+		}
+
+		@Override public URI getAuthorizeEndpoint() {
+			return URI.create(jsonObject.getString(AUTHORIZATION_ENDPOINT));
+		}
+
+		@Override public URI getJwksUri() {
+			return URI.create(jsonObject.getString(JWKS_ENDPOINT));
+		}
+	}
+}
---
+++ token-client/src/main/java/com/sap/cloud/security/xsuaa/client/SpringOidcConfigurationService.java
@@ -0,0 +1,49 @@
+package com.sap.cloud.security.xsuaa.client;
+
+import javax.annotation.Nonnull;
+
+import java.net.URI;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestOperations;
+
+import com.sap.cloud.security.xsuaa.Assertions;
+import com.sap.cloud.security.xsuaa.jwk.JsonWebKeySet;
+import com.sap.cloud.security.xsuaa.jwk.JsonWebKeySetFactory;
+import com.sap.cloud.security.xsuaa.util.UriUtil;
+
+public class SpringOidcConfigurationService implements OidcConfigurationService {
+	private final RestOperations restOperations;
+
+	public SpringOidcConfigurationService(@Nonnull RestOperations restOperations) {
+		Assertions.assertNotNull(restOperations, "restOperations must not be null!");
+		this.restOperations = restOperations;
+	}
+
+	@Override public OAuth2ServiceEndpointsProvider retrieveIssuerEndpoints(@Nonnull URI issuerUri)
+			throws OAuth2ServiceException {
+		Assertions.assertNotNull(issuerUri, "issuerUri must not be null!");
+		URI discoveryEndpointUri = UriUtil.getUriWithPathAppended(issuerUri, DISCOVERY_ENDPOINT_DEFAULT);
+		return retrieveEndpoints(discoveryEndpointUri);
+	}
+
+	@Override public OAuth2ServiceEndpointsProvider retrieveEndpoints(@Nonnull URI discoveryEndpointUri)
+			throws OAuth2ServiceException {
+		Assertions.assertNotNull(discoveryEndpointUri, "discoveryEndpointUri must not be null!");
+		try {
+			ResponseEntity<String> response = restOperations.getForEntity(discoveryEndpointUri, String.class);
+			if (HttpStatus.OK.value() == response.getStatusCode().value()) {
+				return new DefaultOidcConfigurationService.OidcEndpointsProvider(response.getBody());
+			} else {
+				throw OAuth2ServiceException.createWithStatusCodeAndResponseBody("Error retrieving configured oidc endpoints",
+						response.getStatusCodeValue(), response.getBody());
+			}
+		} catch (HttpClientErrorException ex) {
+			throw OAuth2ServiceException.createWithStatusCodeAndResponseBody("Error retrieving configured oidc endpoints",
+					ex.getStatusCode().value(), ex.getResponseBodyAsString());
+		}
+	}
+
+}
