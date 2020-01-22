7e45bd2cd489a709ba57c0538ead12532eaaf6fa
Nena Raab <nena.raab@sap.com>
2020-01-22 13:38:58 +0100

    SAPOfflineTokenServicesCloud should run with RestTemplate

--- java-security/src/main/java/com/sap/cloud/security/adapter/spring/SAPOfflineTokenServicesCloud.java
+++ java-security/src/main/java/com/sap/cloud/security/adapter/spring/SAPOfflineTokenServicesCloud.java
@@ -3,10 +3,13 @@ package com.sap.cloud.security.adapter.spring;
 import com.sap.cloud.security.config.OAuth2ServiceConfiguration;
 import com.sap.cloud.security.token.Token;
 import com.sap.cloud.security.token.XsuaaToken;
-import com.sap.cloud.security.token.validation.CombiningValidator;
 import com.sap.cloud.security.token.validation.ValidationResult;
 import com.sap.cloud.security.token.validation.Validator;
 import com.sap.cloud.security.token.validation.validators.JwtValidatorBuilder;
+import com.sap.cloud.security.xsuaa.client.OAuth2TokenKeyServiceWithCache;
+import com.sap.cloud.security.xsuaa.client.OidcConfigurationServiceWithCache;
+import com.sap.cloud.security.xsuaa.client.SpringOAuth2TokenKeyService;
+import com.sap.cloud.security.xsuaa.client.SpringOidcConfigurationService;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
@@ -14,6 +17,8 @@ import org.springframework.security.oauth2.common.exceptions.InvalidTokenExcepti
 import org.springframework.security.oauth2.provider.AuthorizationRequest;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Nonnull;
 import java.util.HashMap;
@@ -28,8 +33,45 @@ public class SAPOfflineTokenServicesCloud implements ResourceServerTokenServices
 	private final OAuth2ServiceConfiguration serviceConfiguration;
 	private Validator<Token> tokenValidator;
 
+	/**
+	 * This constructor requires a dependency to Apache Rest Client.
+	 * <pre>{@code
+	 * <dependency>
+	 *     <groupId>org.apache.httpcomponents</groupId>
+	 *     <artifactId>httpclient</artifactId>
+	 * </dependency>
+	 * }</pre>
+	 * @param serviceConfiguration the service configuration.
+	 *                             You can use {@link com.sap.cloud.security.config.Environments} in order to load
+	 *                             service configuration from the binding information in your environment.
+	 */
 	public SAPOfflineTokenServicesCloud(OAuth2ServiceConfiguration serviceConfiguration) {
 		this(serviceConfiguration, () -> JwtValidatorBuilder.getInstance(serviceConfiguration).build());
+	}
+
+	/**
+	 * This constructor requires a dependency to Spring web.
+	 * <pre>{@code
+	 * <dependency>
+	 *     <groupId>org.springframework</groupId>
+	 *     <artifactId>spring-web</artifactId>
+	 * </dependency>
+	 * }</pre>
+	 * @param serviceConfiguration the service configuration.
+	 *                             You can use {@link com.sap.cloud.security.config.Environments} in order to load
+	 *                             service configuration from the binding information in your environment.
+	 * @param restOperations the spring rest template
+	 */
+	public SAPOfflineTokenServicesCloud(OAuth2ServiceConfiguration serviceConfiguration, RestOperations restOperations) {
+		this(serviceConfiguration, () -> JwtValidatorBuilder.getInstance(serviceConfiguration)
+				.withOAuth2TokenKeyService(
+						OAuth2TokenKeyServiceWithCache.getInstance()
+								.withTokenKeyService(new SpringOAuth2TokenKeyService(restOperations)))
+				.withOidcConfigurationService(
+						OidcConfigurationServiceWithCache.getInstance()
+								.withOidcConfigurationService(new SpringOidcConfigurationService(restOperations))
+				)
+				.build());
 	}
 
 	SAPOfflineTokenServicesCloud(OAuth2ServiceConfiguration serviceConfiguration,
