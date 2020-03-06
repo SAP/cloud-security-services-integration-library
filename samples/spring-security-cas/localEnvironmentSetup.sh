a7c7828cfe882e5b62c6115bda12876c6a6d0a34
Nena Raab <nena.raab@sap.com>
2020-03-06 18:38:54 +0100

    unify to one service instance (ias or xsuaa)

--- samples/spring-security-cas/localEnvironmentSetup.sh
+++ samples/spring-security-cas/localEnvironmentSetup.sh
@@ -1,4 +1,2 @@
 export VCAP_APPLICATION='{}' # required when cloud profile is active
-export VCAP_SERVICES='{ "identity-beta": [ { "credentials": { "clientid": "7b20f4ec-6b7b-4d8d-aea2-b5fd5604d8a7", "clientsecret": "Abcd1234" }, "name": "spring-security-cas-ias", "plan": "default" } ], "xsuaa": [   {    "binding_name": null,    "credentials": {     "clientid": "sb-java-security-usage!t8065",     "clientsecret": "smO6Y02TmNLOMlkdmR6l4k+K/XA="    },    "name": "spring-security-cas-xsuaa"   }  ] }'
-#TODO
-#export OPA_START='echo "skip OPA Start"'
+export VCAP_SERVICES='{ "myoauthserver": [ { "credentials": { "clientid": "sb-spring-security-cas!t10931", "clientsecret": "Eq/orleqBQk0GnsE6ie2nxomMOk=" }, "name": "spring-security-cas-authn" } ] }'
