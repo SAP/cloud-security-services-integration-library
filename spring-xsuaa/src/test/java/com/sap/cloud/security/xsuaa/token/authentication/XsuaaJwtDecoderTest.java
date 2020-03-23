bd5acf31eec605a6260a26cedb16a53fdd8ea9d6
Daniel Hassler <daniel.hassler@sap.com>
2020-03-23 08:15:59 +0100

    Renamed token file

--- spring-xsuaa/src/test/java/com/sap/cloud/security/xsuaa/token/authentication/XsuaaJwtDecoderTest.java
+++ spring-xsuaa/src/test/java/com/sap/cloud/security/xsuaa/token/authentication/XsuaaJwtDecoderTest.java
@@ -20,7 +20,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @RunWith(SpringRunner.class)
-@TestPropertySource(properties = {
+@TestPropertySource(
+		properties = {
 		"xsuaa.uaadomain=localhost",
 		"xsuaa.clientid=sb-clientId!t0815",
 		"xsuaa.verificationkey=-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAm1QaZzMjtEfHdimrHP3/2Yr+1z685eiOUlwybRVG9i8wsgOUh+PUGuQL8hgulLZWXU5MbwBLTECAEMQbcRTNVTolkq4i67EP6JesHJIFADbK1Ni0KuMcPuiyOLvDKiDEMnYG1XP3X3WCNfsCVT9YoU+lWIrZr/ZsIvQri8jczr4RkynbTBsPaAOygPUlipqDrpadMO1momNCbea/o6GPn38LxEw609ItfgDGhL6f/yVid5pFzZQWb+9l6mCuJww0hnhO6gt6Rv98OWDty9G0frWAPyEfuIW9B+mR/2vGhyU9IbbWpvFXiy9RVbbsM538TCjd5JF2dJvxy24addC4oQIDAQAB-----END PUBLIC KEY-----"
@@ -33,7 +34,7 @@ public class XsuaaJwtDecoderTest {
 
 	@Test
 	public void decode_withVerficationKey() throws IOException {
-		String token = IOUtils.resourceToString("/accessTokenRSA256_verificationKey.txt", StandardCharsets.UTF_8);
+		String token = IOUtils.resourceToString("/accessTokenRSA256WithVerificationKey.txt", StandardCharsets.UTF_8);
 		final JwtDecoder cut = new XsuaaJwtDecoderBuilder(configuration).build();
 
 		final Jwt jwt = cut.decode(token);
