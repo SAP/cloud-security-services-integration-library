b4e85d3ed447e6960ad1c53cdccdcd724113c661
Nena Raab <nena.raab@sap.com>
2020-04-21 13:35:30 +0200

    DefaultADCService sjould implement ping

--- java-cas/src/main/java/com/sap/cloud/security/cas/client/DefaultADCService.java
+++ java-cas/src/main/java/com/sap/cloud/security/cas/client/DefaultADCService.java
@@ -1,8 +1,6 @@
 package com.sap.cloud.security.cas.client;
 
-import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
