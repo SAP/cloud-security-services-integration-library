4345e74e649eb60124f73e8bc366fda6f8f3a8a9
Nena Raab <nena.raab@sap.com>
2020-05-04 15:56:47 +0200

    examples/ideas on how to define generic security checks

--- samples/spring-security-cas/src/main/java/com/sap/cloud/security/samples/SecurityConfiguration.java
+++ samples/spring-security-cas/src/main/java/com/sap/cloud/security/samples/SecurityConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import static org.springframework.http.HttpMethod.*;
+
 @Configuration
 @EnableWebSecurity//(debug = true)
 //@EnableOAuth2Sso
@@ -17,6 +19,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         // keep / open, everything else only with authentication
         http.antMatcher("/**").authorizeRequests()
                 .antMatchers("/health").permitAll()
+                //.antMatchers(GET, "/").hasAuthority("read")
+                //.antMatchers(POST, "/").hasAuthority('write')
+                //.antMatchers("/salesOrders").hasAuthority('read:SalesOrders')
                 .anyRequest().authenticated()
                 .and().oauth2Login(); // TODO use @EnableOAuth2Sso
     }
