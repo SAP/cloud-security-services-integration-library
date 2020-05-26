f050be9c5ffebd99ed66749ba44566972b97f67c
Nena Raab <nena.raab@sap.com>
2020-05-26 13:46:10 +0200

    undo: LocalAuthoritiesExtractor should not support multiple appIds

--- spring-xsuaa/src/test/java/com/sap/cloud/security/xsuaa/extractor/LocalAuthoritiesExtractorTest.java
+++ spring-xsuaa/src/test/java/com/sap/cloud/security/xsuaa/extractor/LocalAuthoritiesExtractorTest.java
@@ -36,20 +36,4 @@ public class LocalAuthoritiesExtractorTest {
                 new SimpleGrantedAuthority("Scope2"));
     }
 
-    @Test
-    public void extractLocalScopesOfTwoApps() {
-        cut = new LocalAuthoritiesExtractor("appId!1234", "appId2!777");
-        assertThat(cut.getAuthorities(token)).containsExactlyInAnyOrder(
-                new SimpleGrantedAuthority("Scope1"),
-                new SimpleGrantedAuthority("Scope2"),
-                new SimpleGrantedAuthority("Scope3"));
-    }
-
-    @Test
-    public void extractLocalScopesOfTwoApps_SameScopeName() {
-        cut = new LocalAuthoritiesExtractor("appId!1234", "appId2!888");
-        assertThat(cut.getAuthorities(token)).containsExactlyInAnyOrder(
-                new SimpleGrantedAuthority("Scope1"),
-                new SimpleGrantedAuthority("Scope2"));
-    }
 }
