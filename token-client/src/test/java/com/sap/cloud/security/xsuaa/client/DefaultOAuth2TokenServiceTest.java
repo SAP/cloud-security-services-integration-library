ea2b045ea2424b5afd4f84e8464c37ba85304e91
Daniel Hassler <daniel.hassler@sap.com>
2020-05-28 16:41:47 +0200

    Implement new test for optional parameters

--- token-client/src/test/java/com/sap/cloud/security/xsuaa/client/DefaultOAuth2TokenServiceTest.java
+++ token-client/src/test/java/com/sap/cloud/security/xsuaa/client/DefaultOAuth2TokenServiceTest.java
@@ -3,25 +3,31 @@ package com.sap.cloud.security.xsuaa.client;
 import com.sap.cloud.security.xsuaa.http.HttpHeaders;
 import com.sap.cloud.security.xsuaa.http.HttpHeadersFactory;
 import com.sap.cloud.security.xsuaa.util.HttpClientTestFactory;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.assertj.core.util.Maps;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.IOException;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
 import java.util.Collections;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DefaultOAuth2TokenServiceTest {
@@ -29,7 +35,7 @@ public class DefaultOAuth2TokenServiceTest {
 	private static final String ACCESS_TOKEN = "abc123";
 	private static final String REFRESH_TOKEN = "def456";
 	private static final String VALID_JSON_RESPONSE = String
-			.format("{expires_in: 1234, access_token: %s, refresh_token: %s}",
+			.format("{expires_in: 10000, access_token: %s, refresh_token: %s}",
 					ACCESS_TOKEN, REFRESH_TOKEN);
 
 	private CloseableHttpClient mockHttpClient;
@@ -46,39 +52,37 @@ public class DefaultOAuth2TokenServiceTest {
 		CloseableHttpResponse response = HttpClientTestFactory.createHttpResponse("{}");
 		when(mockHttpClient.execute(any(HttpPost.class))).thenReturn(response);
 
-		assertThatThrownBy(() -> requestAccessToken())
+		assertThatThrownBy(() -> requestAccessToken(Collections.emptyMap()))
 				.isInstanceOf(OAuth2ServiceException.class)
 				.hasMessageContaining("expires_in");
 	}
 
 	@Test
-	public void httpResponseWithExpiresIn_yieldsExpiresInTokenResponse() throws IOException {
+	public void execute_yieldsTokenResponseWithCorrectData() throws IOException {
 		CloseableHttpResponse response = HttpClientTestFactory.createHttpResponse(VALID_JSON_RESPONSE);
 		when(mockHttpClient.execute(any(HttpPost.class))).thenReturn(response);
 
-		OAuth2TokenResponse re = requestAccessToken();
-
-		assertThat(re.getExpiredAtDate()).isNotNull();
-	}
-
-	@Test
-	public void httpResponseWithToken_yieldsTokenInTokenResponse() throws IOException {
-		CloseableHttpResponse response = HttpClientTestFactory.createHttpResponse(VALID_JSON_RESPONSE);
-		when(mockHttpClient.execute(any(HttpPost.class))).thenReturn(response);
-
-		OAuth2TokenResponse re = requestAccessToken();
+		OAuth2TokenResponse re = requestAccessToken(Collections.emptyMap());
 
 		assertThat(re.getAccessToken()).isEqualTo(ACCESS_TOKEN);
+		assertThat(re.getRefreshToken()).isEqualTo(REFRESH_TOKEN);
+		assertThat(re.getExpiredAt()).isAfter(Instant.now());
 	}
 
 	@Test
-	public void httpResponseWithRefreshToken_yieldsTokenInTokenResponse() throws IOException {
+	public void executeWithAdditionalParameters_putsParametersIntoPostBody() throws IOException {
+		ArgumentCaptor<HttpPost> httpPostCaptor = ArgumentCaptor.forClass(HttpPost.class);
 		CloseableHttpResponse response = HttpClientTestFactory.createHttpResponse(VALID_JSON_RESPONSE);
 		when(mockHttpClient.execute(any(HttpPost.class))).thenReturn(response);
 
-		OAuth2TokenResponse re = requestAccessToken();
+		requestAccessToken(Maps.newHashMap("myKey", "myValue"));
 
-		assertThat(re.getRefreshToken()).isEqualTo(REFRESH_TOKEN);
+		verify(mockHttpClient, times(1)).execute(httpPostCaptor.capture());
+		HttpPost httpPost = httpPostCaptor.getValue();
+		HttpEntity httpEntity = httpPost.getEntity();
+		assertThat(httpEntity).isNotNull();
+		String postBody = IOUtils.toString(httpEntity.getContent(), StandardCharsets.UTF_8);
+		assertThat(postBody).contains("myKey=myValue");
 	}
 
 	@Test
@@ -88,15 +92,14 @@ public class DefaultOAuth2TokenServiceTest {
 				.createHttpResponse(unauthorizedResponseText, HttpStatus.SC_UNAUTHORIZED);
 		when(mockHttpClient.execute(any(HttpPost.class))).thenReturn(response);
 
-		assertThatThrownBy(() -> requestAccessToken())
+		assertThatThrownBy(() -> requestAccessToken(Collections.emptyMap()))
 				.isInstanceOf(OAuth2ServiceException.class)
 				.hasMessageContaining(unauthorizedResponseText);
 	}
 
-	private OAuth2TokenResponse requestAccessToken() throws OAuth2ServiceException {
+	private OAuth2TokenResponse requestAccessToken(Map<String, String> parameters) throws OAuth2ServiceException {
 		URI tokenEndpointUri = URI.create("https://subdomain.myauth.server.com/oauth/token");
 		HttpHeaders withoutAuthorizationHeader = HttpHeadersFactory.createWithoutAuthorizationHeader();
-		Map<String, String> parameters = Collections.emptyMap();
 		return cut.requestAccessToken(tokenEndpointUri, withoutAuthorizationHeader, parameters);
 	}
 
