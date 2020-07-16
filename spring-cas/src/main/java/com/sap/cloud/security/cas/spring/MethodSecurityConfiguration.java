710e9903c0dd230b3fcfa5017d6b033db7bb003b
Daniel Hassler <daniel.hassler@sap.com>
2020-07-16 10:28:08 +0200

    Remove unused import

--- spring-cas/src/main/java/com/sap/cloud/security/cas/spring/MethodSecurityConfiguration.java
+++ spring-cas/src/main/java/com/sap/cloud/security/cas/spring/MethodSecurityConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
