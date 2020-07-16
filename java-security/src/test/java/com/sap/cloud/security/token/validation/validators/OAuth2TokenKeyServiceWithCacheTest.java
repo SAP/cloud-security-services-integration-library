8271aafd1f42dee122a284e4f1658b547e4435f0
Daniel Hassler <daniel.hassler@sap.com>
2020-07-16 15:00:06 +0200

    Add new test case and refactor old ones

--- java-security/src/test/java/com/sap/cloud/security/token/validation/validators/OAuth2TokenKeyServiceWithCacheTest.java
+++ java-security/src/test/java/com/sap/cloud/security/token/validation/validators/OAuth2TokenKeyServiceWithCacheTest.java
@@ -3,6 +3,7 @@ package com.sap.cloud.security.token.validation.validators;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
@@ -13,7 +14,6 @@ import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 import com.github.benmanes.caffeine.cache.Ticker;
 import com.sap.cloud.security.xsuaa.client.OAuth2ServiceException;
@@ -22,11 +22,10 @@ import com.sap.cloud.security.xsuaa.tokenflows.TokenCacheConfiguration;
 import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 public class OAuth2TokenKeyServiceWithCacheTest {
 
-	public static final int CACHE_TIME_IN_SECONDS = 600;
+	public static final TokenCacheConfiguration CACHE_CONFIGURATION = TokenCacheConfiguration.defaultConfiguration();
 
 	OAuth2TokenKeyServiceWithCache cut;
 	OAuth2TokenKeyService tokenKeyServiceMock;
@@ -35,15 +34,15 @@ public class OAuth2TokenKeyServiceWithCacheTest {
 
 	@Before
 	public void setup() throws IOException {
-		tokenKeyServiceMock = Mockito.mock(OAuth2TokenKeyService.class);
-		when(tokenKeyServiceMock.retrieveTokenKeys(TOKEN_KEYS_URI))
+		tokenKeyServiceMock = mock(OAuth2TokenKeyService.class);
+		when(tokenKeyServiceMock.retrieveTokenKeys(any(URI.class)))
 				.thenReturn(IOUtils.resourceToString("/jsonWebTokenKeys.json", StandardCharsets.UTF_8));
 
 		testCacheTicker = new TestCacheTicker();
 		cut = OAuth2TokenKeyServiceWithCache
 				.getInstance(testCacheTicker)
 				.withTokenKeyService(tokenKeyServiceMock)
-				.withCacheTime(CACHE_TIME_IN_SECONDS);
+				.withCacheConfiguration(CACHE_CONFIGURATION);
 	}
 
 	@Test
@@ -74,29 +73,27 @@ public class OAuth2TokenKeyServiceWithCacheTest {
 	@Test
 	public void retrieveTokenKeys() throws OAuth2ServiceException, InvalidKeySpecException, NoSuchAlgorithmException {
 		PublicKey key = cut.getPublicKey(JwtSignatureAlgorithm.RS256, "key-id-0", TOKEN_KEYS_URI);
-		assertThat(String.valueOf(key.getAlgorithm())).isEqualTo("RSA");
 
-		Mockito.verify(tokenKeyServiceMock, times(1))
-				.retrieveTokenKeys(TOKEN_KEYS_URI);
+		assertThat(String.valueOf(key.getAlgorithm())).isEqualTo("RSA");
+		verify(tokenKeyServiceMock, times(1)).retrieveTokenKeys(TOKEN_KEYS_URI);
 	}
 
 	@Test
 	public void getCachedTokenKeys() throws OAuth2ServiceException, InvalidKeySpecException, NoSuchAlgorithmException {
 		PublicKey key = cut.getPublicKey(JwtSignatureAlgorithm.RS256, "key-id-0", TOKEN_KEYS_URI);
-
 		PublicKey cachedKey = cut.getPublicKey(JwtSignatureAlgorithm.RS256, "key-id-0", TOKEN_KEYS_URI);
+
 		assertThat(cachedKey).isNotNull();
 		assertThat(cachedKey).isSameAs(key);
-
-		Mockito.verify(tokenKeyServiceMock, times(1))
-				.retrieveTokenKeys(TOKEN_KEYS_URI);
+		verify(tokenKeyServiceMock, times(1)).retrieveTokenKeys(TOKEN_KEYS_URI);
 	}
 
 	@Test
 	public void retrieveNoTokenKeys_returnsNull()
 			throws OAuth2ServiceException, InvalidKeySpecException, NoSuchAlgorithmException {
-		cut.withTokenKeyService(Mockito.mock(OAuth2TokenKeyService.class));
+		cut.withTokenKeyService(mock(OAuth2TokenKeyService.class));
 		PublicKey key = cut.getPublicKey(JwtSignatureAlgorithm.RS256, "key-id-0", TOKEN_KEYS_URI);
+
 		assertThat(key).isNull();
 	}
 
@@ -114,34 +111,40 @@ public class OAuth2TokenKeyServiceWithCacheTest {
 	public void retrieveTokenKeys_afterCacheWasCleared()
 			throws OAuth2ServiceException, InvalidKeySpecException, NoSuchAlgorithmException {
 		cut.getPublicKey(JwtSignatureAlgorithm.RS256, "key-id-0", TOKEN_KEYS_URI);
-
 		cut.clearCache();
-
 		PublicKey cachedKey = cut.getPublicKey(JwtSignatureAlgorithm.RS256, "key-id-0", TOKEN_KEYS_URI);
-		assertThat(cachedKey).isNotNull();
 
-		Mockito.verify(tokenKeyServiceMock, times(2))
-				.retrieveTokenKeys(TOKEN_KEYS_URI);
+		assertThat(cachedKey).isNotNull();
+		verify(tokenKeyServiceMock, times(2)).retrieveTokenKeys(TOKEN_KEYS_URI);
 	}
 
 	@Test
 	public void retrieveTokenKeys_doesRequestKeysAgainAfterCacheExpired()
 			throws OAuth2ServiceException, InvalidKeySpecException, NoSuchAlgorithmException {
 		cut.getPublicKey(JwtSignatureAlgorithm.RS256, "key-id-0", TOKEN_KEYS_URI);
-		testCacheTicker.advance(CACHE_TIME_IN_SECONDS, TimeUnit.SECONDS);
+		testCacheTicker.advance(CACHE_CONFIGURATION.getCacheDuration()); // just expired
 		cut.getPublicKey(JwtSignatureAlgorithm.RS256, "key-id-0", TOKEN_KEYS_URI);
-		Mockito.verify(tokenKeyServiceMock, times(2)).retrieveTokenKeys(any());
+
+		verify(tokenKeyServiceMock, times(2)).retrieveTokenKeys(any());
 	}
 
 	@Test
-	public void retrieveTokenKeysFromAnotherEndpoint()
+	public void retrieveTokenKeysForNewKeyId()
 			throws OAuth2ServiceException, InvalidKeySpecException, NoSuchAlgorithmException {
 		cut.getPublicKey(JwtSignatureAlgorithm.RS256, "key-id-0", TOKEN_KEYS_URI);
-		cut.getPublicKey(JwtSignatureAlgorithm.RS256, "key-id-1", TOKEN_KEYS_URI);
+		cut.getPublicKey(JwtSignatureAlgorithm.RS256, "not-seen-yet", TOKEN_KEYS_URI);
+
+		verify(tokenKeyServiceMock, times(2)).retrieveTokenKeys(any());
+	}
+
+
+	@Test
+	public void retrieveTokenKeysForNewEndpoint()
+			throws OAuth2ServiceException, InvalidKeySpecException, NoSuchAlgorithmException {
+		cut.getPublicKey(JwtSignatureAlgorithm.RS256, "key-id-0", TOKEN_KEYS_URI);
 		cut.getPublicKey(JwtSignatureAlgorithm.RS256, "key-id-0", URI.create("http://another/url"));
 
-		Mockito.verify(tokenKeyServiceMock, times(2))
-				.retrieveTokenKeys(any());
+		verify(tokenKeyServiceMock, times(2)).retrieveTokenKeys(any());
 	}
 
 	private class TestCacheTicker implements  Ticker {
@@ -152,8 +155,8 @@ public class OAuth2TokenKeyServiceWithCacheTest {
 			return elapsed;
 		}
 
-		public void advance(long duration, TimeUnit unit) {
-			this.elapsed = elapsed + unit.toNanos(duration);
+		public void advance(Duration duration) {
+			this.elapsed = elapsed + duration.toNanos();
 		}
 	}
 }
