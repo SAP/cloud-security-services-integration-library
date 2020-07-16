8d6551caa3ebf82de6b3273fbba8d300fc315639
Daniel Hassler <daniel.hassler@sap.com>
2020-07-16 10:17:05 +0200

    Add AutoConfigureAfter and fix type in ConditionalOnBean

--- spring-cas/src/main/java/com/sap/cloud/security/cas/spring/MethodSecurityConfiguration.java
+++ spring-cas/src/main/java/com/sap/cloud/security/cas/spring/MethodSecurityConfiguration.java
@@ -2,16 +2,19 @@ package com.sap.cloud.security.cas.spring;
 
 import com.sap.cloud.security.cas.client.AdcService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@AutoConfigureAfter(name = "com.sap.cloud.security.xsuaa.autoconfiguration.XsuaaAutoConfiguration")
 public class MethodSecurityConfiguration extends GlobalMethodSecurityConfiguration {
 
     @Autowired
@@ -26,7 +29,7 @@ public class MethodSecurityConfiguration extends GlobalMethodSecurityConfigurati
     }
 
     @Bean
-    @ConditionalOnBean(type = "com.sap.cloud.security.xsuaa.autoconfiguration.XsuaaAutoConfiguration")
+    @ConditionalOnBean(type = "com.sap.cloud.security.xsuaa.XsuaaServiceConfiguration")
     ExpressionHandlerService expressionHandlerXsuaa() {
         return () -> AdcSpringSecurityExpressionHandlerXsuaa.getInstance(adcService);
     }
