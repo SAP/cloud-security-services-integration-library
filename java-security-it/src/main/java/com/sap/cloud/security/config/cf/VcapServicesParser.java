9a0343bab080740cb2896c611b93ca4cc9727b48
Daniel Hassler <daniel.hassler@sap.com>
2020-08-20 10:23:55 +0200

    Turn VcapServicesParser into a builder
    To support setVerificationKey fluently.

--- java-security-it/src/main/java/com/sap/cloud/security/config/cf/VcapServicesParser.java
+++ java-security-it/src/main/java/com/sap/cloud/security/config/cf/VcapServicesParser.java
@@ -19,6 +19,17 @@ public class VcapServicesParser {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(VcapServicesParser.class);
 
+	private final OAuth2ServiceConfigurationBuilder oAuth2ServiceConfigurationBuilder;
+
+	private VcapServicesParser(OAuth2ServiceConfiguration oAuth2ServiceConfiguration) {
+		checkProperties(oAuth2ServiceConfiguration);
+		this.oAuth2ServiceConfigurationBuilder  = OAuth2ServiceConfigurationBuilder
+				.fromConfiguration(oAuth2ServiceConfiguration)
+				.withProperty(CFConstants.XSUAA.VERIFICATION_KEY, null)
+				.withProperty(CFConstants.XSUAA.UAA_DOMAIN, "localhost")
+				.withUrl("http://localhost");
+	}
+
 	/**
 	 * Reads the contents of a classpath resource given by {@param resourceName} and turns
 	 * it into a {@link OAuth2ServiceConfiguration}.
@@ -27,35 +38,30 @@ public class VcapServicesParser {
 	 * @return the parsed {@link OAuth2ServiceConfiguration}.
 	 * @throws JsonParsingException if the resource cannot be read or contains invalid data.
 	 */
-	public OAuth2ServiceConfiguration fromFile(String resourceName) {
+	public static VcapServicesParser fromFile(String resourceName) {
 		String vcapServicesJson = read(resourceName);
 		List<OAuth2ServiceConfiguration> configurations = findConfigurationsForService(Service.XSUAA, vcapServicesJson);
 		if (configurations.size() > 1) {
 			LOGGER.warn("More than one binding found for service '{}'. Taking first one!", Service.XSUAA);
 		}
 		OAuth2ServiceConfiguration oAuth2ServiceConfiguration = configurations.get(0);
-		checkClientSecretNotProvided(oAuth2ServiceConfiguration);
-		return overrideProperties(oAuth2ServiceConfiguration);
+		return new VcapServicesParser(oAuth2ServiceConfiguration);
 	}
 
-	private OAuth2ServiceConfiguration overrideProperties(OAuth2ServiceConfiguration oAuth2ServiceConfiguration) {
+	public OAuth2ServiceConfiguration createConfiguration() {
+		return oAuth2ServiceConfigurationBuilder.build();
+	}
+
+	private void checkProperties(OAuth2ServiceConfiguration oAuth2ServiceConfiguration) {
 		if (!nullOrEmpty(oAuth2ServiceConfiguration.getProperty(CFConstants.XSUAA.VERIFICATION_KEY))) {
 			LOGGER.warn("Ignoring verification key from service binding!");
 		}
-		return OAuth2ServiceConfigurationBuilder.fromConfiguration(oAuth2ServiceConfiguration)
-				.withProperty(CFConstants.XSUAA.VERIFICATION_KEY, null)
-				.withProperty(CFConstants.XSUAA.UAA_DOMAIN, "localhost")
-				.withUrl("http://localhost")
-				.build();
-	}
-
-	private void checkClientSecretNotProvided(OAuth2ServiceConfiguration oAuth2ServiceConfiguration) {
 		if (!nullOrEmpty(oAuth2ServiceConfiguration.getClientSecret())) {
 			throw new JsonParsingException("Client secret must not be provided!");
 		}
 	}
 
-	private List<OAuth2ServiceConfiguration> findConfigurationsForService(Service service, String vcapServicesJson) {
+	private static List<OAuth2ServiceConfiguration> findConfigurationsForService(Service service, String vcapServicesJson) {
 		Map<Service, List<OAuth2ServiceConfiguration>> serviceToConfigurations = CFEnvParser
 				.loadAll(vcapServicesJson, "{}");
 		List<OAuth2ServiceConfiguration> oAuth2ServiceConfigurations = serviceToConfigurations
@@ -73,7 +79,7 @@ public class VcapServicesParser {
 	 * @param resourceName
 	 * @return the file as string.
 	 */
-	private String read(String resourceName) {
+	private static String  read(String resourceName) {
 		try {
 			return IOUtils.resourceToString(resourceName, StandardCharsets.UTF_8);
 		} catch (IOException e) {
@@ -81,7 +87,7 @@ public class VcapServicesParser {
 		}
 	}
 
-	private boolean nullOrEmpty(String value) {
+	private static boolean nullOrEmpty(String value) {
 		return value == null || value.isEmpty();
 	}
 }
--- java-security-it/src/test/java/com/sap/cloud/security/config/cf/VcapServicesParserTest.java
+++ java-security-it/src/test/java/com/sap/cloud/security/config/cf/VcapServicesParserTest.java
@@ -2,7 +2,6 @@ package com.sap.cloud.security.config.cf;
 
 import com.sap.cloud.security.config.OAuth2ServiceConfiguration;
 import com.sap.cloud.security.json.JsonParsingException;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.net.URI;
@@ -14,17 +13,11 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class VcapServicesParserTest {
 
-	private VcapServicesParser vcapServicesParser;
-
-	@BeforeEach
-	void setUp() {
-		vcapServicesParser = new VcapServicesParser();
-	}
-
 	@Test
 	void fromFile_loadsConfiguration() {
-		OAuth2ServiceConfiguration oAuth2ServiceConfiguration = vcapServicesParser
-				.fromFile("/vcapServices/vcapSimple.json");
+		OAuth2ServiceConfiguration oAuth2ServiceConfiguration = VcapServicesParser
+				.fromFile("/vcapServices/vcapSimple.json")
+				.createConfiguration();
 
 		assertThat(oAuth2ServiceConfiguration.getClientId()).isEqualTo("clientId");
 		assertThat(oAuth2ServiceConfiguration.getProperty(CFConstants.SERVICE_PLAN)).isEqualTo("broker");
@@ -32,8 +25,9 @@ class VcapServicesParserTest {
 
 	@Test
 	void fromFile_loadsConfiguration_PropertiesAreOverridden() {
-		OAuth2ServiceConfiguration oAuth2ServiceConfiguration = vcapServicesParser
-				.fromFile("/vcapServices/vcapSimple.json");
+		OAuth2ServiceConfiguration oAuth2ServiceConfiguration = VcapServicesParser
+				.fromFile("/vcapServices/vcapSimple.json")
+				.createConfiguration();
 
 		assertThat(oAuth2ServiceConfiguration.getProperty(VERIFICATION_KEY)).isNull();
 		assertThat(oAuth2ServiceConfiguration.getClientSecret()).isNull();
@@ -43,21 +37,21 @@ class VcapServicesParserTest {
 
 	@Test
 	void fromFile_resourceDoesNotExits_throwsException() {
-		assertThatThrownBy(() -> vcapServicesParser.fromFile("/doesNotExist.json"))
+		assertThatThrownBy(() -> VcapServicesParser.fromFile("/doesNotExist.json"))
 				.isInstanceOf(JsonParsingException.class)
 				.hasMessageContaining("Resource not found: /doesNotExist.json");
 	}
 
 	@Test
 	void fromFile_rejectsConfigurationWithClientCredentials() {
-		assertThatThrownBy(() -> vcapServicesParser.fromFile("/vcapServices/vcapWithClientSecret.json"))
+		assertThatThrownBy(() -> VcapServicesParser.fromFile("/vcapServices/vcapWithClientSecret.json"))
 				.isInstanceOf(JsonParsingException.class)
 				.hasMessageContaining("Client secret must not be provided!");
 	}
 
 	@Test
 	void fromFile_rejectsVcapWithoutBinding() {
-		assertThatThrownBy(() -> vcapServicesParser.fromFile("/vcapServices/vcapWithoutBinding.json"))
+		assertThatThrownBy(() -> VcapServicesParser.fromFile("/vcapServices/vcapWithoutBinding.json"))
 				.isInstanceOf(JsonParsingException.class)
 				.hasMessageContaining("No supported binding found in VCAP_SERVICES!");
 	}
