8b24309ea8f9ea9f45a1fa94c4ecdccb82ee8eec
Daniel Hassler <daniel.hassler@sap.com>
2020-08-24 14:07:54 +0200

    Add static token keys to template

--- java-security-test/src/test/java/com/sap/cloud/security/test/SecurityTestRuleTest.java
+++ java-security-test/src/test/java/com/sap/cloud/security/test/SecurityTestRuleTest.java
@@ -62,7 +62,7 @@ public class SecurityTestRuleTest {
 			assertThat(response.getStatusLine().getStatusCode()).isEqualTo(HttpStatus.SC_OK);
 
 			List<JsonObject> tokenKeys = new DefaultJsonObject(readContent(response)).getJsonObjects("keys");
-			assertThat(tokenKeys).hasSize(2);
+			assertThat(tokenKeys).hasSize(3);
 			String publicKeyFromTokenKeys = tokenKeys.get(0).getAsString("value");
 			assertThat(publicKeyFromTokenKeys).isEqualTo(expEncodedKey);
 			assertThat(publicKeyFromTokenKeys)
