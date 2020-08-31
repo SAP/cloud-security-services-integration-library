7ed82b6e78cc87e2664084814c44a35005af9f6f
Daniel Hassler <daniel.hassler@sap.com>
2020-08-31 13:25:38 +0200

    Create correct IAS jwks entry
    As defined in RFC 7515 section-4.1.6 the x5c entry
    must be DER PKIX certificate of the corresponding
    private key.
    It has been created with the following command
    ```
    openssl req -new -x509 -nodes -key privateKey.txt -days 4096
    ```

--- java-security-test/src/main/java/com/sap/cloud/security/test/JwtGenerator.java
+++ java-security-test/src/main/java/com/sap/cloud/security/test/JwtGenerator.java
@@ -28,7 +28,7 @@ import static com.sap.cloud.security.token.TokenHeader.*;
 public class JwtGenerator {
 	public static final Instant NO_EXPIRE_DATE = new GregorianCalendar(2190, 11, 31).getTime().toInstant();
 	public static final String DEFAULT_KEY_ID = "default-kid";
-	public static final String DEFAULT_KEY_ID_IAS = "ias-kid";
+	public static final String DEFAULT_KEY_ID_IAS = "default-ias-kid";
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(JwtGenerator.class);
 	private static final String DEFAULT_JWKS_URL = "http://localhost/token_keys";
