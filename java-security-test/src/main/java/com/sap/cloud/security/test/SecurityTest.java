b58586335bf33f672f964aef3bb2a3ac2dfa231b
Daniel Hassler <daniel.hassler@sap.com>
2020-09-24 11:18:27 +0200

    Set key id depending on service

--- java-security-test/src/main/java/com/sap/cloud/security/test/SecurityTest.java
+++ java-security-test/src/main/java/com/sap/cloud/security/test/SecurityTest.java
@@ -340,9 +340,13 @@ public class SecurityTest {
 				.encodeToString(((RSAPublicKey) keys.getPublic()).getModulus().toByteArray());
 		String encodedPublicKey = Base64.getEncoder().encodeToString(keys.getPublic().getEncoded());
 		return IOUtils.resourceToString("/token_keys_template.json", StandardCharsets.UTF_8)
-				.replace("$kid", JwtGenerator.DEFAULT_KEY_ID)
+				.replace("$kid", getKeyId())
 				.replace("$public_key", encodedPublicKey)
 				.replace("$modulus", encodedPublicKeyModulus);
+	}
+
+	private String getKeyId() {
+		return this.service == Service.IAS ? JwtGenerator.DEFAULT_KEY_ID_IAS : JwtGenerator.DEFAULT_KEY_ID;
 	}
 
 	String createDefaultOidcConfigurationResponse() throws IOException {
