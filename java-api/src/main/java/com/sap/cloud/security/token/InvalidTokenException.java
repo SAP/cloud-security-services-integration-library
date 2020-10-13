0f11229e1bf103e147bde5f3ff2276da66421769
nenaraab <nenaraab@users.noreply.github.com>
2020-10-13 16:38:48 +0200

    Update InvalidTokenException.java

--- java-api/src/main/java/com/sap/cloud/security/token/InvalidTokenException.java
+++ java-api/src/main/java/com/sap/cloud/security/token/InvalidTokenException.java
@@ -1,7 +1,7 @@
 package com.sap.cloud.security.token;
 
 /**
- * Runtime exception during client id retrieval from token.
+ * Runtime exception during token validation.
  */
 public class InvalidTokenException extends RuntimeException {
 	public InvalidTokenException(String message) {
