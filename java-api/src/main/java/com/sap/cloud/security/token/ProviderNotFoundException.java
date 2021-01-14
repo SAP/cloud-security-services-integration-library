2a0a1b87d3c2fad1e3ed52b3d65e9a095ff69868
liga-oz <liga.ozolina@sap.com>
2021-01-14 09:57:51 +0100

    add missing java doc refactoring of TokenProvider class

--- java-api/src/main/java/com/sap/cloud/security/token/ProviderNotFoundException.java
+++ java-api/src/main/java/com/sap/cloud/security/token/ProviderNotFoundException.java
@@ -1,5 +1,8 @@
 package com.sap.cloud.security.token;
 
+/**
+ * A dedicated runtime exception for missing implementations in {@link java.util.ServiceLoader} context
+ */
 public class ProviderNotFoundException extends RuntimeException {
 
 	public ProviderNotFoundException() {
