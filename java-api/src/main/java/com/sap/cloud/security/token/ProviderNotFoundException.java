bff32dbaf88af5b60f6e288da703714a394a8f2e
liga-oz <liga.ozolina@sap.com>
2021-01-15 14:32:08 +0100

    apply formatter

--- java-api/src/main/java/com/sap/cloud/security/token/ProviderNotFoundException.java
+++ java-api/src/main/java/com/sap/cloud/security/token/ProviderNotFoundException.java
@@ -1,7 +1,8 @@
 package com.sap.cloud.security.token;
 
 /**
- * A dedicated runtime exception for missing implementations in {@link java.util.ServiceLoader} context
+ * A dedicated runtime exception for missing implementations in
+ * {@link java.util.ServiceLoader} context
  */
 public class ProviderNotFoundException extends RuntimeException {
 
