7c498d76f2ba19cd68911d7bb15944ec204439a7
Nena Raab <nena.raab@sap.com>
2021-01-29 09:48:54 +0100

    introduce sap.spring.security.hybrid.sync_securitycontex

--- spring-security/src/test/java/com/sap/cloud/security/spring/autoconfig/SecurityContextAutoConfigurationTest.java
+++ spring-security/src/test/java/com/sap/cloud/security/spring/autoconfig/SecurityContextAutoConfigurationTest.java
@@ -10,12 +10,14 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 class SecurityContextAutoConfigurationTest {
 
     private final WebApplicationContextRunner runner = new WebApplicationContextRunner()
+            .withPropertyValues("sap.spring.security.hybrid.sync_securitycontext:true")
             .withConfiguration(AutoConfigurations.of(SecurityContextAutoConfiguration.class));
 
     @Test
     void autoConfigurationActive() {
         runner.run(context -> assertNotNull(context.getBean("methodInvokingFactoryBean")));
     }
+
     @Test
     void autoConfigurationDisabledByProperty() {
         runner.withPropertyValues("sap.spring.security.hybrid.auto:false").run((context) -> assertFalse(context.containsBean("methodInvokingFactoryBean")));
