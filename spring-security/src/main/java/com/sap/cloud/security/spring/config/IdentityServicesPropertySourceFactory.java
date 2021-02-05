f65c2ca0092c4f828e4f7644efd3ebe26706be51
Nena Raab <nena.raab@sap.com>
2021-02-05 16:42:45 +0100

    fix javadoc

--- spring-security/src/main/java/com/sap/cloud/security/spring/config/IdentityServicesPropertySourceFactory.java
+++ spring-security/src/main/java/com/sap/cloud/security/spring/config/IdentityServicesPropertySourceFactory.java
@@ -17,7 +17,7 @@ import org.springframework.core.io.support.PropertySourceFactory;
 
 
 /**
- * Part of Auto Configuration {@link com.sap.cloud.security.spring.autoconfig.HybridIdentityServicesAutoConfiguration}
+ * Part of Auto Configuration {@code HybridIdentityServicesAutoConfiguration}
  *
  * <h2>Example Usage</h2>
  *
