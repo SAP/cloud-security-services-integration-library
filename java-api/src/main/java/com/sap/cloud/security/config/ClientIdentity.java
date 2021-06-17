f4d056bb30f6d4decdc430f5446c68a3760b24bc
liga-oz <liga.ozolina@sap.com>
2021-06-17 11:58:10 +0300

    add javadoc

--- java-api/src/main/java/com/sap/cloud/security/config/ClientIdentity.java
+++ java-api/src/main/java/com/sap/cloud/security/config/ClientIdentity.java
@@ -4,24 +4,52 @@ package com.sap.cloud.security.config;
  * Represents xsuaa client identity
  */
 public interface ClientIdentity {
+	/**
+	 * Client id of identity service instance.
+	 *
+	 * @return client identifier
+	 */
 	String getId();
 
+	/**
+	 * Returns true, if the mandatory attributes in ClientIdentity class are filled for the specified authentication method i.e X.509 or client secret
+	 *
+	 * @return the boolean
+	 */
 	default boolean isValid() {
-		return true;
-	}
-
-	default boolean isCertificateBased() {
 		return false;
 	}
 
+	/**
+	 *  Returns true if ClientIdentity is certificate based.
+	 *
+	 * @return the boolean
+	 */
+	default boolean isCertificateBased() { return false; }
+
+	/**
+	 * Client secret of identity service instance.
+	 *
+	 * @return client secret
+	 */
 	default String getSecret() {
 		return null;
 	}
 
+	/**
+	 * PEM encoded certificate chain.
+	 *
+	 * @return certificate chain
+	 */
 	default String getCertificate() {
 		return null;
 	}
 
+	/**
+	 * PEM encoded private key the certificate is signed with.
+	 *
+	 * @return private key
+	 */
 	default String getKey() {
 		return null;
 	}
