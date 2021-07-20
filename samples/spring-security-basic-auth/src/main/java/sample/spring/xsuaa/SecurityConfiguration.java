d0048e74917c1f1f875e0311674b69659b06bf7c
liga-oz <liga.ozolina@sap.com>
2021-07-20 12:25:19 +0300

    adapt sample for k8s

--- samples/spring-security-basic-auth/src/main/java/sample/spring/xsuaa/SecurityConfiguration.java
+++ samples/spring-security-basic-auth/src/main/java/sample/spring/xsuaa/SecurityConfiguration.java
@@ -12,10 +12,14 @@ import com.sap.cloud.security.xsuaa.extractor.AuthenticationMethod;
 import com.sap.cloud.security.xsuaa.extractor.DefaultAuthenticationInformationExtractor;
 import com.sap.cloud.security.xsuaa.extractor.TokenBrokerResolver;
 import com.sap.cloud.security.xsuaa.token.TokenAuthenticationConverter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cache.Cache;
 import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -31,6 +35,8 @@ import java.util.concurrent.TimeUnit;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(SecurityConfiguration.class);
 
 	@Autowired
 	XsuaaServiceConfiguration xsuaaServiceConfiguration;
@@ -48,7 +54,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		// @formatter:off
 		http.authorizeRequests()
 				.antMatchers("/hello-token").hasAuthority("Display")
-				.anyRequest().authenticated()
+				.antMatchers("/health").permitAll()
+				.anyRequest().denyAll()
 			.and()
 				.sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -78,6 +85,118 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		TokenAuthenticationConverter converter = new TokenAuthenticationConverter(xsuaaServiceConfiguration);
 		converter.setLocalScopeAsAuthorities(true);
 		return converter;
+	}
+
+	@Bean
+	public XsuaaServiceConfiguration xsuaaServiceConfiguration() {
+		LOGGER.info("auto-configures XsuaaServiceConfigurationK8s");
+		return new XsuaaServiceConfigurationK8s();
+	}
+
+	@Configuration
+	@ConfigurationProperties
+	public class XsuaaServiceConfigurationK8s implements XsuaaServiceConfiguration {
+
+		private String clientId;
+
+		private String clientSecret;
+
+		private String url;
+
+		private String uaaDomain;
+
+		private String identityZoneId;
+
+		private String appid;
+
+		private String privateKey;
+
+		private String certificate;
+
+		private String verificationKey;
+
+		private String credentialType;
+
+		private String certUrl;
+
+		@Override
+		public String getUaaUrl() {
+			LOGGER.info("uaa url: {}", url);
+			return url;
+		}
+
+		@Override
+		public String getAppId() {
+			LOGGER.info("app id: {}", appid);
+			return appid;
+		}
+
+		@Override
+		public String getUaaDomain() {
+			LOGGER.info("uaa domain: {}", uaaDomain);
+			return uaaDomain;
+		}
+
+		@Override
+		public String getVerificationKey() {
+			return verificationKey;
+		}
+
+		@Override
+		public String getClientId() {
+			LOGGER.info("clientid: {}", clientId);
+			return clientId;
+		}
+
+		@Override
+		public String getClientSecret() {
+			LOGGER.info("client secret: {}", clientSecret);
+			return clientSecret;
+		}
+
+		public void setClientId(String clientId) {
+			this.clientId = clientId;
+		}
+
+		public void setClientSecret(String clientSecret) {
+			this.clientSecret = clientSecret;
+		}
+
+		public void setUrl(String url) {
+			this.url = url;
+		}
+
+		public void setUaaDomain(String uaadomain) {
+			this.uaaDomain = uaadomain;
+		}
+
+		public void setIdentityZoneId(String identityZoneId) {
+			this.identityZoneId = identityZoneId;
+		}
+
+		public void setXsappname(String xsappname) {
+			this.appid = xsappname;
+		}
+
+		public void setPrivateKey(String privateKey) {
+			this.privateKey = privateKey;
+		}
+
+		public void setCertificate(String certificate) {
+			this.certificate = certificate;
+		}
+
+		public void setVerificationKey(String verificationKey) {
+			this.verificationKey = verificationKey;
+		}
+
+		public void setCredentialType(String credentialType) {
+			this.credentialType = credentialType;
+		}
+
+		public void setCertUrl(String certUrl) {
+			this.certUrl = certUrl;
+		}
 	}
 
 }
