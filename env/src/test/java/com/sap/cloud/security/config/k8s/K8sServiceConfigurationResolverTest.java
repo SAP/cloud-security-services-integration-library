02b5ae6ad1584848ff95b646b896cd6975ac14a6
liga-oz <liga.ozolina@sap.com>
2021-08-25 14:17:01 +0200

    fix sporadic test result

--- env/src/test/java/com/sap/cloud/security/config/k8s/K8sServiceConfigurationResolverTest.java
+++ env/src/test/java/com/sap/cloud/security/config/k8s/K8sServiceConfigurationResolverTest.java
@@ -6,7 +6,6 @@
 package com.sap.cloud.security.config.k8s;
 
 import com.sap.cloud.security.config.Service;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
@@ -15,9 +14,7 @@ import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 import java.io.File;
 
 import static com.sap.cloud.security.config.k8s.K8sConstants.*;
-import static com.sap.cloud.security.config.k8s.K8sConstants.SM_CONFIG_PATH;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SystemStubsExtension.class)
 class K8sServiceConfigurationResolverTest {
@@ -25,22 +22,20 @@ class K8sServiceConfigurationResolverTest {
 	private static K8sServiceConfigurationResolver cut;
 	public static final String ABSOLUTE_PATH = new File("src/test/resources").getAbsolutePath();
 
-	@BeforeAll
-	static void setUp(EnvironmentVariables environmentVariables) {
-		environmentVariables.set(XSUAA_CONFIG_PATH, ABSOLUTE_PATH + "/k8s/xsuaa");
-		environmentVariables.set(IAS_CONFIG_PATH, ABSOLUTE_PATH + "/k8s/ias");
-		environmentVariables.set(SM_CONFIG_PATH, ABSOLUTE_PATH + "/k8s/service-manager");
-
-		cut = new K8sServiceConfigurationResolver();
-	}
-
 	@Test
-	void loadServiceManagerConfig() {
+	void loadServiceManagerConfig(EnvironmentVariables environmentVariables) {
+		environmentVariables.set(SM_CONFIG_PATH, ABSOLUTE_PATH + "/k8s/service-manager");
+		cut = new K8sServiceConfigurationResolver();
+
 		assertEquals("smClientId", cut.loadServiceManagerConfig().getClientId());
 	}
 
 	@Test
-	void loadOauth2ServiceConfig() {
+	void loadOauth2ServiceConfig(EnvironmentVariables environmentVariables) {
+		environmentVariables.set(XSUAA_CONFIG_PATH, ABSOLUTE_PATH + "/k8s/xsuaa");
+		environmentVariables.set(IAS_CONFIG_PATH, ABSOLUTE_PATH + "/k8s/ias");
+		cut = new K8sServiceConfigurationResolver();
+
 		assertEquals(2, cut.loadOauth2ServiceConfig(Service.XSUAA).size());
 		assertEquals(2, cut.loadOauth2ServiceConfig(Service.IAS).size());
 		assertEquals("xsuaaClientId",
