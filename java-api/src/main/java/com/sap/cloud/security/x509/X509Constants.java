eb89c463c44af7b607bedc81eb02e1dc43ad33da
liga-oz <liga.ozolina@sap.com>
2021-11-12 16:18:18 +0100

    introduce X509 constants

---
+++ java-api/src/main/java/com/sap/cloud/security/x509/X509Constants.java
@@ -0,0 +1,14 @@
+/**
+ * SPDX-FileCopyrightText: 2018-2021 SAP SE or an SAP affiliate company and Cloud Security Client Java contributors
+ * <p>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.sap.cloud.security.x509;
+
+public final class X509Constants {
+
+    private X509Constants() {}
+
+    public static final String X5T_VALIDATOR_ENABLED = "X5T_VALIDATOR_ENABLED";
+    public static final String ACCEPT_CALLERS_BOUND_TO_SAME_ID_SERVICE = "ACCEPT_CALLERS_BOUND_TO_SAME_ID_SERVICE";
+}
