ce809c2d0fe4b1bd97bf1061898b6c2c8dca4835
liga-oz <liga.ozolina@sap.com>
2021-11-15 15:28:58 +0100

    add x-fwd-client-cert constant

--- java-api/src/main/java/com/sap/cloud/security/x509/X509Constants.java
+++ java-api/src/main/java/com/sap/cloud/security/x509/X509Constants.java
@@ -12,4 +12,5 @@ public final class X509Constants {
 
 	public static final String X5T_VALIDATOR_ENABLED = "X5T_VALIDATOR_ENABLED";
 	public static final String ACCEPT_CALLERS_BOUND_TO_SAME_ID_SERVICE = "ACCEPT_CALLERS_BOUND_TO_SAME_ID_SERVICE";
+	public static final String FWD_CLIENT_CERT_HEADER = "x-forwarded-client-cert";
 }
