fcce3bdfc56da18e7829abdbdc7f6e2b9167dc2e
akib6074@gmail.com <akib.javed@apsissolutions.com>
2023-03-23 21:10:10 +0600

    potentially fixed the flaky issue

--- spring-security/src/main/java/com/sap/cloud/security/spring/token/authentication/AuthenticationToken.java
+++ spring-security/src/main/java/com/sap/cloud/security/spring/token/authentication/AuthenticationToken.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.util.Assert;
+import java.lang.reflect.Field;
 
 import java.util.Collection;
 import java.util.Objects;
@@ -59,17 +60,35 @@ public class AuthenticationToken extends JwtAuthenticationToken {
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj != null && this.getClass() != obj.getClass()) {
-			return false;
-		}
-		if (obj == null) {
-			return false;
-		}
 		AuthenticationToken that = (AuthenticationToken) obj;
-		return this.token.equals(that.token) && this.getAuthorities().equals(that.getAuthorities());
+		return compareObjects(this.token,that.token) && this.getAuthorities().equals(that.getAuthorities());
+	}
+
+	public static boolean compareObjects(Object obj1, Object obj2) {
+		if (obj2 == null) {
+			return false;
+		}
+		if (!obj1.getClass().equals(obj2.getClass())) {
+			return false;
+		}
+		Field[] fields = obj1.getClass().getDeclaredFields();
+		for (Field field : fields) {
+			field.setAccessible(true);
+			try {
+				Object value1 = field.get(obj1);
+				Object value2 = field.get(obj2);
+				if (value1 == null || value2 == null) {
+					if (value1 != value2) {
+						return false;
+					}
+				} else if (!value1.equals(value2)) {
+					return false;
+				}
+			} catch (IllegalAccessException e) {
+				// handle exception
+			}
+		}
+		return true;
 	}
 
 	@Override
