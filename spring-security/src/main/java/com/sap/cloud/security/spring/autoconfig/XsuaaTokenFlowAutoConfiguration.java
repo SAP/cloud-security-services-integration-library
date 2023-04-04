5f0f17cc18439305fc67347b14761e1906b6eef2
liga-oz <liga.ozolina@sap.com>
2023-04-04 14:17:48 +0200

    use HttpClient as Bean without RestTemplate for token-client

--- spring-security/src/main/java/com/sap/cloud/security/spring/autoconfig/XsuaaTokenFlowAutoConfiguration.java
+++ spring-security/src/main/java/com/sap/cloud/security/spring/autoconfig/XsuaaTokenFlowAutoConfiguration.java
@@ -5,15 +5,16 @@
  */
 package com.sap.cloud.security.spring.autoconfig;
 
+import com.sap.cloud.security.client.HttpClientFactory;
 import com.sap.cloud.security.config.ClientIdentity;
-import com.sap.cloud.security.config.CredentialType;
 import com.sap.cloud.security.spring.config.XsuaaServiceConfiguration;
 import com.sap.cloud.security.spring.config.XsuaaServiceConfigurations;
+import com.sap.cloud.security.xsuaa.client.DefaultOAuth2TokenService;
 import com.sap.cloud.security.xsuaa.client.OAuth2ServiceEndpointsProvider;
 import com.sap.cloud.security.xsuaa.client.OAuth2TokenService;
 import com.sap.cloud.security.xsuaa.client.XsuaaDefaultEndpoints;
-import com.sap.cloud.security.xsuaa.client.XsuaaOAuth2TokenService;
 import com.sap.cloud.security.xsuaa.tokenflows.XsuaaTokenFlows;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
@@ -22,12 +23,10 @@ import org.springframework.boot.autoconfigure.condition.AnyNestedCondition;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.*;
-import org.springframework.core.type.AnnotatedTypeMetadata;
-import org.springframework.web.client.RestOperations;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.Nonnull;
 
 /**
  * {@link EnableAutoConfiguration Auto-configuration} for default beans used by
@@ -60,59 +59,25 @@ public class XsuaaTokenFlowAutoConfiguration {
 
 	@Bean
 	@Conditional(PropertyConditions.class)
-	public XsuaaTokenFlows xsuaaTokenFlows(RestOperations xsuaaRestOperations) {
+	public XsuaaTokenFlows xsuaaTokenFlows(CloseableHttpClient httpClient) {
 		logger.debug("auto-configures XsuaaTokenFlows using {} based restOperations",
 				xsuaaConfig.getClientIdentity().isCertificateBased() ? "certificate" : "client secret");
 		OAuth2ServiceEndpointsProvider endpointsProvider = new XsuaaDefaultEndpoints(xsuaaConfig);
 		ClientIdentity clientIdentity = xsuaaConfig.getClientIdentity();
-		OAuth2TokenService oAuth2TokenService = new XsuaaOAuth2TokenService(xsuaaRestOperations);
+		OAuth2TokenService oAuth2TokenService = new DefaultOAuth2TokenService(httpClient);
 		return new XsuaaTokenFlows(oAuth2TokenService, endpointsProvider, clientIdentity);
 	}
 
 	/**
-	 * Creates a {@link RestOperations} instance if the application has not defined
-	 * any.
+	 * Creates a {@link CloseableHttpClient} instance configured with the ClientIdentity provided. Conditional on missing CloseableHttpClient Bean.
 	 *
-	 * @return the {@link RestOperations} instance.
+	 * @return the {@link CloseableHttpClient} instance.
 	 */
 	@Bean
-	@Conditional({ OnSecretCredentialTypeCondition.class, NoClientCertificateCondition.class })
-	@ConditionalOnMissingBean
-	public RestOperations restOperations() {
-		logger.warn("In productive environment provide a well configured client secret based RestOperations bean");
-		return new RestTemplate();
-	}
-
-	/**
-	 * Creates a certificate based {@link RestOperations} instance if the
-	 * application has not defined any.
-	 *
-	 * @return the {@link RestOperations} instance.
-	 */
-	@Bean
-	@ConditionalOnClass(name = "org.apache.hc.client5.http.impl.classic.CloseableHttpClient")
-	@ConditionalOnMissingBean
-	public RestOperations mtlsRestOperations(XsuaaServiceConfiguration xsuaaConfig) {
-		logger.warn("In productive environment provide a well configured certificate based RestOperations bean");
-		return SpringHttpClient.getInstance().create(xsuaaConfig.getClientIdentity());
-	}
-
-	private static class OnSecretCredentialTypeCondition implements Condition {
-		@Override
-		public boolean matches(ConditionContext context, @Nonnull AnnotatedTypeMetadata metadata) {
-			CredentialType credentialType = CredentialType
-					.from(context.getEnvironment().getProperty("sap.security.services.xsuaa.credential-type"));
-			return credentialType == CredentialType.BINDING_SECRET || credentialType == CredentialType.INSTANCE_SECRET
-					|| credentialType == null;
-		}
-	}
-
-	private static class NoClientCertificateCondition implements Condition {
-		@Override
-		public boolean matches(ConditionContext context, @Nonnull AnnotatedTypeMetadata metadata) {
-			return context.getEnvironment().getProperty("sap.security.services.xsuaa.certificate") == null &&
-					context.getEnvironment().getProperty("sap.security.services.xsuaa[0].certificate") == null;
-		}
+	@ConditionalOnMissingBean(type = "org.apache.http.impl.client.CloseableHttpClient")
+	public CloseableHttpClient defaultHttpClient(XsuaaServiceConfiguration xsuaaConfig) {
+		logger.info("If the performance for token validation is degrading provide your own well configured HttpClientFactory implementation");
+		return HttpClientFactory.create(xsuaaConfig.getClientIdentity());
 	}
 
 	private static class PropertyConditions extends AnyNestedCondition {
