06fc3bfada7c80c2f7c5505487aadb0c0a58c22c
Manuel Fink <manuel.fink@sap.com>
2023-04-13 15:23:17 +0200

    Fix IAS configuration parsing exception when domains are missing Replace VcapServicesParser.java with ServiceBindingMapper to reduce custom env parsing code in java-security-test

--- env/src/main/java/com/sap/cloud/security/config/ServiceBindingEnvironment.java
+++ env/src/main/java/com/sap/cloud/security/config/ServiceBindingEnvironment.java
@@ -8,10 +8,7 @@ package com.sap.cloud.security.config;
 import com.sap.cloud.environment.servicebinding.api.DefaultServiceBindingAccessor;
 import com.sap.cloud.environment.servicebinding.api.ServiceBinding;
 import com.sap.cloud.environment.servicebinding.api.ServiceBindingAccessor;
-import com.sap.cloud.environment.servicebinding.api.TypedMapView;
 import com.sap.cloud.security.json.DefaultJsonObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -24,7 +21,6 @@ import java.util.stream.Stream;
 
 import static com.sap.cloud.security.config.Service.IAS;
 import static com.sap.cloud.security.config.Service.XSUAA;
-import static com.sap.cloud.security.config.ServiceConstants.IAS.DOMAINS;
 import static com.sap.cloud.security.config.ServiceConstants.SERVICE_PLAN;
 import static com.sap.cloud.security.config.ServiceConstants.VCAP_APPLICATION;
 
@@ -34,7 +30,6 @@ import static com.sap.cloud.security.config.ServiceConstants.VCAP_APPLICATION;
  * and supplies accessor methods for service-specific configuration objects parsed from these bindings. *
  */
 public class ServiceBindingEnvironment implements Environment {
-    private static final Logger LOGGER = LoggerFactory.getLogger(ServiceBindingEnvironment.class);
     private final ServiceBindingAccessor serviceBindingAccessor;
     private UnaryOperator<String> environmentVariableReader = System::getenv;
     private Map<Service, Map<ServiceConstants.Plan, OAuth2ServiceConfiguration>> serviceConfigurations;
@@ -133,7 +128,7 @@ public class ServiceBindingEnvironment implements Environment {
                 .collect(Collectors.toMap(Function.identity(), service ->
                         serviceBindings.stream()
                         .filter(b -> service.equals(Service.from(b.getServiceName().orElse(""))))
-                        .map(ServiceBindingEnvironment::mapServiceBindingToConfigurationBuilder)
+                        .map(ServiceBindingMapper::mapToOAuth2ServiceConfigurationBuilder)
                         .filter(Objects::nonNull)
                         .map(builder -> builder.runInLegacyMode(runInLegacyMode()))
                         .map(OAuth2ServiceConfigurationBuilder::build)
@@ -147,39 +142,6 @@ public class ServiceBindingEnvironment implements Environment {
      */
     private void clearServiceConfigurations() {
         this.serviceConfigurations = null;
-    }
-
-    /**
-     * Parses a service binding by extracting the configuration information and passing it to a configuration builder.
-     * @return a new {@link OAuth2ServiceConfigurationBuilder} that is configured based on the given {@link ServiceBinding}.
-     */
-    @Nullable
-    private static OAuth2ServiceConfigurationBuilder mapServiceBindingToConfigurationBuilder(ServiceBinding b) {
-        if (b.getServiceName().isEmpty()) {
-            LOGGER.error("Ignores Service Binding with name {} as service name is not provided.", b.getName());
-            return null;
-        }
-
-        final Service service = Service.from(b.getServiceName().get());
-        if (service == null) {
-            LOGGER.error(
-                    "Service name {} is unknown. Could not create a OAuth2ServiceConfiguration from service binding.",
-                    b.getServiceName().get());
-            return null;
-        }
-
-        OAuth2ServiceConfigurationBuilder builder = OAuth2ServiceConfigurationBuilder.forService(service)
-                .withProperties(TypedMapView.ofCredentials(b).getEntries(String.class))
-                .withProperty(SERVICE_PLAN,
-                        b.getServicePlan().orElse(ServiceConstants.Plan.APPLICATION.name()).toUpperCase());
-
-        if (IAS.equals(service)) {
-            List<String> domains = TypedMapView.ofCredentials(b).getListView(DOMAINS).getItems(String.class);
-            LOGGER.info("first domain : {}", domains.get(0));
-            builder.withDomains(domains.toArray(new String[] {}));
-        }
-
-        return builder;
     }
 
     private boolean runInLegacyMode() {
