e03989dca227fd016d0ca521ce0ed7673dd1f103
Manuel Fink <manuel.fink@sap.com>
2023-04-18 11:47:21 +0200

    move shebang line to beginning of k8s-deploy-and-test.sh

--- samples/k8s-deploy-and-test.sh
+++ samples/k8s-deploy-and-test.sh
@@ -1,6 +1,6 @@
+#!/usr/bin/env bash
 # SPDX-FileCopyrightText: 2018-2023 SAP SE or an SAP affiliate company and Cloud Security Client Java contributors
 # SPDX-License-Identifier: Apache-2.0
-#!/usr/bin/env bash
 # Prerequisites:
 #   * have logged in to docker
 #   * valid kubeconfig.yml stored in samples root folder
