f0386d03d29b7d2d6021c97178788ed312025f9c
liga-oz <liga.ozolina@sap.com>
2023-07-17 18:58:48 +0200

    adapt AbstractToken zoneId method to use getAppTid

--- java-security/src/main/java/com/sap/cloud/security/token/AbstractToken.java
+++ java-security/src/main/java/com/sap/cloud/security/token/AbstractToken.java
@@ -24,7 +24,8 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.regex.Pattern;
 
-import static com.sap.cloud.security.token.TokenClaims.*;
+import static com.sap.cloud.security.token.TokenClaims.EXPIRATION;
+import static com.sap.cloud.security.token.TokenClaims.NOT_BEFORE;
 import static com.sap.cloud.security.token.TokenClaims.XSUAA.*;
 
 /**
@@ -170,7 +171,7 @@ public abstract class AbstractToken implements Token {
 
 	@Override
 	public String getZoneId() {
-		return getClaimAsString(SAP_GLOBAL_ZONE_ID);
+		return getAppTid();
 	}
 
 	@Override
