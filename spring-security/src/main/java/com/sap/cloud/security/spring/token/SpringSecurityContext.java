043dffb09d03bb65aae17f1d0262d04748319c1c
liga-oz <liga.ozolina@sap.com>
2023-10-04 11:27:18 +0200

    remove logging of user data

--- spring-security/src/main/java/com/sap/cloud/security/spring/token/SpringSecurityContext.java
+++ spring-security/src/main/java/com/sap/cloud/security/spring/token/SpringSecurityContext.java
@@ -57,7 +57,7 @@ public class SpringSecurityContext {
 			return (Token) principal;
 		}
 		throw new AccessDeniedException(
-				"Access forbidden: SecurityContextHolder does not contain a principal of type 'Token' " + principal);
+				"Access forbidden: SecurityContextHolder does not contain a principal of type 'Token' instead " + principal.getClass());
 	}
 
 	/**
