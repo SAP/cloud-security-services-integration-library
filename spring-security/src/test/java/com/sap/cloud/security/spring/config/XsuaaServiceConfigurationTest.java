400a69e60b13f678bdb9875c303595d1b8dc37ec
Manuel Fink <manuel.fink@sap.com>
2023-12-13 16:21:44 +0100

    Revert XsuaaServiceConfiguration(s)Test to no longer test with IdentityServicesPropertySourceFactory
    This reverts commit 4af5956c

--- spring-security/src/test/java/com/sap/cloud/security/spring/config/XsuaaServiceConfigurationTest.java
+++ spring-security/src/test/java/com/sap/cloud/security/spring/config/XsuaaServiceConfigurationTest.java
@@ -5,44 +5,26 @@
  */
 package com.sap.cloud.security.spring.config;
 
-import com.sap.cloud.environment.servicebinding.SapVcapServicesServiceBindingAccessor;
-import com.sap.cloud.security.config.ServiceBindingEnvironment;
-import org.apache.commons.io.IOUtils;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 
-import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
-
-
-@SpringBootTest(classes = { SingleXsuaaConfigurationFromFile.class })
 class XsuaaServiceConfigurationTest {
 
-	static ServiceBindingEnvironment env;
+	private final ApplicationContextRunner runner = new ApplicationContextRunner();
 
-	@Autowired
-	XsuaaServiceConfiguration xsuaaConfig;
-
-	@BeforeAll
-	static void setup() throws IOException {
-		String serviceBindingJson = IOUtils.resourceToString("/singleXsuaaAndIasBinding.json", UTF_8);
-		env = new ServiceBindingEnvironment(new SapVcapServicesServiceBindingAccessor(any -> serviceBindingJson));
+	@EnableConfigurationProperties(XsuaaServiceConfiguration.class)
+	static class EnablePropertiesConfiguration {
 	}
 
 	@Test
 	void configuresXsuaaServiceConfiguration() {
-		ConfigurationUtil.assertConfigsAreEqual(xsuaaConfig, env.getXsuaaConfiguration());
-    }
-
+		runner.withUserConfiguration(EnablePropertiesConfiguration.class)
+				.withPropertyValues("sap.security.services.xsuaa.url:http://localhost",
+						"sap.security.services.xsuaa.uaadomain:localhost", "sap.security.services.xsuaa.clientid:cid")
+				.run(context -> assertEquals("http://localhost",
+						context.getBean(XsuaaServiceConfiguration.class).getUrl().toString()));
+	}
 }
-
-@Configuration
-@PropertySource(factory = IdentityServicesPropertySourceFactory.class, value = { "classpath:singleXsuaaAndIasBinding.json" })
-@EnableConfigurationProperties(XsuaaServiceConfiguration.class)
-class SingleXsuaaConfigurationFromFile {}
