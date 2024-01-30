e3d5cab2f1f87062e01c0cbaa49f307d9b2e4e8f
Marie von Seggern <marie.von.seggern@sap.com>
2024-01-30 13:31:55 +0100

    parseJwt Method in ReactiveHybridJwtDecoder corrected

--- spring-security/src/main/java/com/sap/cloud/security/spring/token/authentication/ReactiveHybridJwtDecoder.java
+++ spring-security/src/main/java/com/sap/cloud/security/spring/token/authentication/ReactiveHybridJwtDecoder.java
@@ -1,6 +1,7 @@
 package com.sap.cloud.security.spring.token.authentication;
 
 import com.sap.cloud.security.token.Token;
+import com.sap.cloud.security.token.TokenClaims;
 import com.sap.cloud.security.token.validation.CombiningValidator;
 import com.sap.cloud.security.token.validation.ValidationResult;
 import org.slf4j.Logger;
@@ -9,9 +10,10 @@ import org.springframework.security.oauth2.jwt.BadJwtException;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
-import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 import reactor.core.publisher.Mono;
+
+import java.time.Instant;
 
 public class ReactiveHybridJwtDecoder implements ReactiveJwtDecoder {
 
@@ -26,9 +28,9 @@ public class ReactiveHybridJwtDecoder implements ReactiveJwtDecoder {
 
     @Override
     public Mono<Jwt> decode(String encodedToken) throws JwtException {
-        return Mono.just(encodedToken)
+        return Mono.justOrEmpty(encodedToken)
                 .filter(token -> StringUtils.hasText(token))
-                .switchIfEmpty(Mono.error(new BadJwtException("encoded Token must neither be null nor empty String.")))
+                .switchIfEmpty(Mono.error(new BadJwtException("Encoded Token must neither be null nor empty String.")))
                 .map(Token::create)
                 .flatMap(token -> {
                     Mono<Jwt> jwt = parseJwt(token);
@@ -50,14 +52,17 @@ public class ReactiveHybridJwtDecoder implements ReactiveJwtDecoder {
                     return validationResult
                             .filter(vr -> !vr.isErroneous())
                             .switchIfEmpty(Mono.error(new BadJwtException("The token is invalid: " + validationResult.block().getErrorDescription())))
-                            .doOnNext(result -> logger.debug("Token issued by {} service was succesfully validated.", token.getService()))
+                            .doOnNext(result -> logger.debug("Token issued by {} service was successfully validated.", token.getService()))
                             .then(jwt);
                 })
                 .onErrorMap(RuntimeException.class, ex -> new BadJwtException("Error initializing JWT decoder: " + ex.getMessage(), ex));
     }
 
     public static Mono<Jwt> parseJwt(Token token) {
-        return Mono.just(new Jwt(token.getTokenValue(), token.getNotBefore(), token.getExpiration(),
-                token.getHeaders(), token.getClaims()));
+        Instant issuedAt = token.hasClaim(TokenClaims.XSUAA.ISSUED_AT)
+                ? Instant.ofEpochSecond(Long.parseLong(token.getClaimAsString(TokenClaims.XSUAA.ISSUED_AT)))
+                : null;
+        return Mono.just(new Jwt(token.getTokenValue(), issuedAt,
+                token.getExpiration(), token.getHeaders(), token.getClaims()));
     }
 }
