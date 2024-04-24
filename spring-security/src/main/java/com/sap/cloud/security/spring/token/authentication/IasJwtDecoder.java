f932d6bb55d0eaf48d259199d37be2e7181c8c57
Līga <72249435+liga-oz@users.noreply.github.com>
2024-04-24 11:03:42 +0200

    fix fwd client cert bug with a missing cert header (#1521)
    Signed-off-by: liga-oz <liga.ozolina@sap.com>

--- spring-security/src/main/java/com/sap/cloud/security/spring/token/authentication/IasJwtDecoder.java
+++ spring-security/src/main/java/com/sap/cloud/security/spring/token/authentication/IasJwtDecoder.java
@@ -50,7 +50,7 @@ public class IasJwtDecoder implements JwtDecoder {
 
 			if (servletRequestAttributes != null) {
 				HttpServletRequest request = servletRequestAttributes.getRequest();
-				String clientCert = String.valueOf(request.getHeader(FWD_CLIENT_CERT_HEADER));
+				String clientCert = request.getHeader(FWD_CLIENT_CERT_HEADER);
 				if (clientCert != null) {
 					SecurityContext.setClientCertificate(X509Certificate.newCertificate(clientCert));
 				}
