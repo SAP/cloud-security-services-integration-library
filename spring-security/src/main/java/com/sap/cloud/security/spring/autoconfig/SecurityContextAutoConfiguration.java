73b879bec2fd19aef96f3fb175427536860237f5
Marc Becker <marc.becker@sap.com>
2024-04-30 15:32:30 +0200

    Improve the custom SecurityContext Strategy registration (#1526)
    * Improve the custom SecurityContext Strategy registration
    
    * Use ServletContextInitializer to hook early into the initialization phase

--- spring-security/src/main/java/com/sap/cloud/security/spring/autoconfig/SecurityContextAutoConfiguration.java
+++ spring-security/src/main/java/com/sap/cloud/security/spring/autoconfig/SecurityContextAutoConfiguration.java
@@ -5,15 +5,23 @@
  */
 package com.sap.cloud.security.spring.autoconfig;
 
-import org.springframework.beans.factory.config.MethodInvokingFactoryBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextHolderStrategy;
 
+import com.sap.cloud.security.spring.token.authentication.JavaSecurityContextHolderStrategy;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
 /**
  * {@link EnableAutoConfiguration} uses a
  * {@link com.sap.cloud.security.spring.token.authentication.JavaSecurityContextHolderStrategy}, which keeps the
@@ -25,17 +33,36 @@ import org.springframework.security.core.context.SecurityContextHolderStrategy;
  */
 @Configuration
 @ConditionalOnProperty(name = "sap.spring.security.hybrid.auto", havingValue = "true", matchIfMissing = true)
+@ConditionalOnWebApplication
+@ConditionalOnClass(ServletContextInitializer.class)
 public class SecurityContextAutoConfiguration {
 
 	@Bean
 	@ConditionalOnMissingBean(SecurityContextHolderStrategy.class)
 	@ConditionalOnProperty(name = "sap.spring.security.hybrid.sync_securitycontext", havingValue = "true", matchIfMissing = true)
-	public MethodInvokingFactoryBean methodInvokingFactoryBean() {
-		MethodInvokingFactoryBean methodInvokingFactoryBean = new MethodInvokingFactoryBean();
-		methodInvokingFactoryBean.setTargetClass(SecurityContextHolder.class);
-		methodInvokingFactoryBean.setTargetMethod("setStrategyName");
-		methodInvokingFactoryBean
-				.setArguments("com.sap.cloud.security.spring.token.authentication.JavaSecurityContextHolderStrategy");
-		return methodInvokingFactoryBean;
+	SecurityContextSetter securityContextSetter() {
+		return new SecurityContextSetter();
 	}
+
+	static class SecurityContextSetter implements InitializingBean, ServletContextInitializer, Ordered {
+
+		@Override
+		public void afterPropertiesSet() throws Exception {
+			if (!(SecurityContextHolder.getContextHolderStrategy() instanceof JavaSecurityContextHolderStrategy)) {
+				SecurityContextHolder.setContextHolderStrategy(new JavaSecurityContextHolderStrategy());
+			}
+		}
+
+		@Override
+		public void onStartup(ServletContext servletContext) throws ServletException {
+			// empty, used to hook early into the initialization phase
+		}
+
+		@Override
+		public int getOrder() {
+			return Ordered.HIGHEST_PRECEDENCE;
+		}
+
+	}
+
 }
