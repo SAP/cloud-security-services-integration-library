df238a3200d5256bb4c0f53b773ab298e07849e3
Marc Becker <marc.becker@sap.com>
2024-04-30 11:07:18 +0200

    Improve the custom SecurityContext Strategy registration
    The library now registers a security context strategy by default. In CAP Java we ran into situations where this registration happened too late.
    The effect of this was, that SecurityConfigs used an obsolete SecurityContextStrategy (which stored by Spring Security as a member variable in the SecurityConfig) and the library set a new strategy in the static holder class.
    As the SecurityConfigs stored the authentication objects in the obsolete strategy, CAP wasn't able anymore to get the current authentication from the static holder class.
    
    This PR tries to avoid this situation, by ensuring that the auto-configuration has a high precedence and is run early in the application initialization.
    Additional this PR avoids reflection usage to set the strategy, as reflection is not really required.

--- spring-security/src/main/java/com/sap/cloud/security/spring/autoconfig/SecurityContextAutoConfiguration.java
+++ spring-security/src/main/java/com/sap/cloud/security/spring/autoconfig/SecurityContextAutoConfiguration.java
@@ -5,14 +5,17 @@
  */
 package com.sap.cloud.security.spring.autoconfig;
 
-import org.springframework.beans.factory.config.MethodInvokingFactoryBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextHolderStrategy;
+
+import com.sap.cloud.security.spring.token.authentication.JavaSecurityContextHolderStrategy;
 
 /**
  * {@link EnableAutoConfiguration} uses a
@@ -24,18 +27,14 @@ import org.springframework.security.core.context.SecurityContextHolderStrategy;
  * property {@code sap.spring.security.hybrid.auto = false}.
  */
 @Configuration
-@ConditionalOnProperty(name = "sap.spring.security.hybrid.auto", havingValue = "true", matchIfMissing = true)
-public class SecurityContextAutoConfiguration {
+@ConditionalOnProperty(name = { "sap.spring.security.hybrid.auto", "sap.spring.security.hybrid.sync_securitycontext"}, havingValue = "true", matchIfMissing = true)
+@ConditionalOnMissingBean(SecurityContextHolderStrategy.class)
+@AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE) // ensures that bean is initialized before other configurations
+public class SecurityContextAutoConfiguration implements InitializingBean {
 
-	@Bean
-	@ConditionalOnMissingBean(SecurityContextHolderStrategy.class)
-	@ConditionalOnProperty(name = "sap.spring.security.hybrid.sync_securitycontext", havingValue = "true", matchIfMissing = true)
-	public MethodInvokingFactoryBean methodInvokingFactoryBean() {
-		MethodInvokingFactoryBean methodInvokingFactoryBean = new MethodInvokingFactoryBean();
-		methodInvokingFactoryBean.setTargetClass(SecurityContextHolder.class);
-		methodInvokingFactoryBean.setTargetMethod("setStrategyName");
-		methodInvokingFactoryBean
-				.setArguments("com.sap.cloud.security.spring.token.authentication.JavaSecurityContextHolderStrategy");
-		return methodInvokingFactoryBean;
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		SecurityContextHolder.setContextHolderStrategy(new JavaSecurityContextHolderStrategy());
 	}
+
 }
