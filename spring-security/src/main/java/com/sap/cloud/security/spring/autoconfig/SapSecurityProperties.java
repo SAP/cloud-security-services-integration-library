9d5cb37868efa1d5fd20686114b9552423633879
Līga <72249435+liga-oz@users.noreply.github.com>
2024-06-20 13:12:11 +0200

    Extend autoconfigurations for hybridJwtDecoders prooftoken check (#1566)
    * introduce new autoconfig for prooftoken and spring conditions class
    
    Signed-off-by: liga-oz <liga.ozolina@sap.com>
    
    ---------
    
    Signed-off-by: liga-oz <liga.ozolina@sap.com>

---
+++ spring-security/src/main/java/com/sap/cloud/security/spring/autoconfig/SapSecurityProperties.java
@@ -0,0 +1,16 @@
+/**
+ * SPDX-FileCopyrightText: 2018-2023 SAP SE or an SAP affiliate company and Cloud Security Client Java contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.sap.cloud.security.spring.autoconfig;
+
+public class SapSecurityProperties {
+
+	static final String SAP_SECURITY_SERVICES_IDENTITY_DOMAINS = "sap.security.services.identity.domains";
+	static final String SAP_SECURITY_SERVICES_XSUAA_UAADOMAIN = "sap.security.services.xsuaa.uaadomain";
+	static final String SAP_SECURITY_SERVICES_XSUAA_0_UAADOMAIN = "sap.security.services.xsuaa[0].uaadomain";
+	static final String SAP_SPRING_SECURITY_IDENTITY_PROOFTOKEN = "sap.spring.security.identity.prooftoken";
+	static final String SAP_SPRING_SECURITY_HYBRID = "sap.spring.security.hybrid.auto";
+	private SapSecurityProperties() {
+	}
+}
